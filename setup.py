b15861e36dd5e7a0ec8886b167591330eab852de
Sylvain Bellemare <sbellem@gmail.com>
2016-02-09 21:50:55 +0100

    Add setup.py for empty release

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+BigchainDB: A Scalable Blockchain Database
+
+For full docs visit https://bigchaindb.readthedocs.org
+
+"""
+from setuptools import setup
+
+setup(
+    name='BigchainDB',
+    version='0.0.0.dev1',
+    description='BigchainDB: A Scalable Blockchain Database',
+    long_description=__doc__,
+    url='https://github.com/BigchainDB/bigchaindb/',
+    author='BigchainDB Contributors',
+    author_email='dev@bigchaindb.com',
+    license='AGPLv3',
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=[],
+)
