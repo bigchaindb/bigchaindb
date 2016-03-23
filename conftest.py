ee38202a27615fb95d77a5a3421187c96ba7f324
diminator <dimi.dejonghe@gmail.com>
2016-03-23 19:14:59 +0100

    import keys from cryptoconditions set ed25519 as default

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ import pytest
 
 DB_NAME = 'bigchain_test_{}'.format(os.getpid())
 
-CONFIG = {
+CONFIG_ECDSA = {
     'database': {
         'name': DB_NAME
     },
@@ -25,8 +25,22 @@ CONFIG = {
 }
 
 # Test user. inputs will be created for this user. Cryptography Keys
-USER_PRIVATE_KEY = 'GmRZxQdQv7tooMijXytQkexKuFN6mJocciJarAmMwTX2'
-USER_PUBLIC_KEY = 'r3cEu8GNoz8rYpNJ61k7GqfR8VEvdUbtyHce8u1kaYwh'
+USER_PRIVATE_KEY_ECDSA = 'GmRZxQdQv7tooMijXytQkexKuFN6mJocciJarAmMwTX2'
+USER_PUBLIC_KEY_ECDSA = 'r3cEu8GNoz8rYpNJ61k7GqfR8VEvdUbtyHce8u1kaYwh'
+
+CONFIG_ED25519 = {
+    'database': {
+        'name': DB_NAME
+    },
+    'keypair': {
+        'private': '3wssdnSNsZYLvvQwuag5QNQnSfc5N38KV1ZeAoeHQQVe59N7vReJwXWANf5nncGxW63UzR4qHHv6DJhyLs9arJng',
+        'public': '4spEuJCR6UNkS9Qyz6QwseU3ENRaypkcVgGKDeqfg8Ha'
+    }
+}
+
+# Test user. inputs will be created for this user. Cryptography Keys
+USER_PRIVATE_KEY_ED25519 = '3RZ3Kn8JbzyNwqzDwhU4dkZFFcwVkfgjhKiiqybfabxFAaANZqPemEudxTYMKfkbrHADTGCkvR7uQHSjihsXLbcM'
+USER_PUBLIC_KEY_ED25519 = '2XJT5M6D3fYhvDbgcHmGMUcrGeZ9MtCWGqQZZVXghjv9'
 
 
 @pytest.fixture
@@ -37,17 +51,17 @@ def restore_config(request, node_config):
 
 @pytest.fixture(scope='module')
 def node_config():
-    return copy.deepcopy(CONFIG)
+    return copy.deepcopy(CONFIG_ED25519)
 
 
 @pytest.fixture
 def user_private_key():
-    return USER_PRIVATE_KEY
+    return USER_PRIVATE_KEY_ED25519
 
 
 @pytest.fixture
 def user_public_key():
-    return USER_PUBLIC_KEY
+    return USER_PUBLIC_KEY_ED25519
 
 
 @pytest.fixture
