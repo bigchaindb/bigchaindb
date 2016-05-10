37c34f7383a6e52c386af5abb105f0a5f334fcf3
Rodolphe Marques <rodolphe@ascribe.io>
2016-05-10 11:48:27 +0200

    remove tmp file

--- benchmarking-tests/hostlist.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""A list of the public DNS names of all the nodes in this
-BigchainDB cluster/federation.
-"""
-
-from __future__ import unicode_literals
-
-public_dns_names = ['ec2-52-58-110-9.eu-central-1.compute.amazonaws.com', 'ec2-52-58-154-94.eu-central-1.compute.amazonaws.com', 'ec2-52-58-166-93.eu-central-1.compute.amazonaws.com']
