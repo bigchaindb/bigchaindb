814d8a16635f85a7d18515d13f12472246cf0e2a
ryan <hendersonry@gmail.com>
2016-05-18 17:09:26 +0200

    wait for indexes

--- tests/db/conftest.py
+++ tests/db/conftest.py
@@ -54,7 +54,7 @@ def setup_database(request, node_config):
     r.db(db_name).table('bigchain').index_create('transaction_id', r.row['block']['transactions']['id'],
                                                  multi=True).run()
 
-    r.db(db_name).table('bigchain').index_wait('transaction_id')
+    r.db(db_name).table('bigchain').index_wait('transaction_id').run()
 
     def fin():
         print('Deleting `{}` database'.format(db_name))
