ac680cf5e98bd7160e927a8ea6aebd01579c1da9
Sylvain Bellemare <sbellem@gmail.com>
2016-06-28 14:19:07 +0200

    Test consensus (#398)
    * Remove unused import
    
    * Simplify and group the imports
    
    * Add extra space (pep 8)
    
    * Remove NotImplementedError
    
    the class BaseConsensusRules implements verify_vote_signature
    
    * Add test module for consensus module

--- bigchaindb/consensus.py
+++ bigchaindb/consensus.py
@@ -1,9 +1,6 @@
-import copy
 from abc import ABCMeta, abstractmethod
 
-import bigchaindb.exceptions as exceptions
-from bigchaindb import util
-from bigchaindb import crypto
+from bigchaindb import crypto, exceptions, util
 
 
 class AbstractConsensusRules(metaclass=ABCMeta):
@@ -101,7 +98,7 @@ class AbstractConsensusRules(metaclass=ABCMeta):
             bool: True if the votes's required signature data is present
                 and correct, False otherwise.
         """
-        raise NotImplementedError
+
 
 class BaseConsensusRules(AbstractConsensusRules):
     """Base consensus rules for Bigchain.
