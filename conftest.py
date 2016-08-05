98b24a9ecfecd8fbf9e53d1ace6fd65803380878
tim <tim@ascribe.io>
2016-08-05 10:13:34 +0200

    Add Tests for tx serialization logic

--- conftest.py
+++ conftest.py
@@ -32,6 +32,11 @@ USER_VERIFYING_KEY = 'JEAkEJqLbbgDRAtMm8YAjGp759Aq2qTn9eaEHUj2XePE'
 CC_FULFILLMENT_URI = 'cf:0:'
 CC_CONDITION_URI = 'cc:0:3:47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU:0'
 
+PAYLOAD = {
+    'msg': 'Hello BigchainDB!'
+}
+PAYLOAD_ID = '872fa6e6f46246cd44afdb2ee9cfae0e72885fb0910e2bcf9a5a2a4eadb417b8'
+
 
 # We need this function to avoid loading an existing
 # conf file located in the home of the user running
@@ -85,6 +90,45 @@ def cond_uri():
 
 
 @pytest.fixture
-def ffill(ffill_uri):
+def cc_ffill(ffill_uri, user_vk):
     from cryptoconditions import Fulfillment
     return Fulfillment.from_uri(ffill_uri)
+
+
+@pytest.fixture
+def ffill(cc_ffill, user_vk):
+    from bigchaindb.transaction import Fulfillment
+    return Fulfillment(cc_ffill, [user_vk])
+
+
+@pytest.fixture
+def default_ffill(user_vk):
+    from bigchaindb.transaction import Fulfillment
+    return Fulfillment.gen_default(user_vk)
+
+
+@pytest.fixture
+def default_cond(default_ffill):
+    return default_ffill.gen_condition()
+
+
+@pytest.fixture
+def payload():
+    return PAYLOAD
+
+
+@pytest.fixture
+def payload_id():
+    return PAYLOAD_ID
+
+
+@pytest.fixture
+def data(payload, payload_id):
+    from bigchaindb.transaction import Data
+    return Data(payload, payload_id)
+
+
+@pytest.fixture
+def tx(default_ffill, default_cond):
+    from bigchaindb.transaction import Transaction
+    return Transaction(Transaction.CREATE, [default_ffill], [default_cond])
--- test_transaction.py
+++ test_transaction.py
@@ -1,118 +1,195 @@
 from pytest import raises
 
 
-def test_fulfillment_serialization(ffill, user_vk):
-    from bigchaindb.transaction import (
-        Fulfillment,
-    )
+def test_fulfillment_serialization(ffill_uri, user_vk):
+    from bigchaindb.transaction import Fulfillment
+    from cryptoconditions import Fulfillment as CCFulfillment
 
-    ffill = Fulfillment(ffill, [user_vk])
-    ffill_dict = ffill.to_dict()
-    assert ffill_dict['owners_before'] == [user_vk]
-    assert ffill_dict['input'] == None
-    assert ffill_dict['fulfillment'] == ffill.fulfillment.serialize_uri()
-    assert ffill_dict['fid'] == 0
-
-
-def test_fulfillment_deserialization(ffill_uri, user_vk):
-    from bigchaindb.transaction import (
-        Fulfillment,
-    )
-
-    ffill_dict = {
+    expected = {
         'owners_before': [user_vk],
         'fulfillment': ffill_uri,
+        'details': CCFulfillment.from_uri(ffill_uri).to_dict(),
         'fid': 0,
         'input': None,
     }
+    ffill = Fulfillment(CCFulfillment.from_uri(ffill_uri), [user_vk])
 
-    assert Fulfillment.from_dict(ffill_dict).to_dict() == ffill_dict
+    assert ffill.to_dict() == expected
 
 
-def test_invalid_fulfillment_initialization(ffill, user_vk):
-    from bigchaindb.transaction import (
-        Fulfillment,
-    )
+def test_fulfillment_deserialization_with_uri(ffill_uri, user_vk):
+    from bigchaindb.transaction import Fulfillment
+    from cryptoconditions import Fulfillment as CCFulfillment
+
+    expected = Fulfillment(CCFulfillment.from_uri(ffill_uri), [user_vk])
+    ffill = {
+        'owners_before': [user_vk],
+        'fulfillment': ffill_uri,
+        'details': CCFulfillment.from_uri(ffill_uri).to_dict(),
+        'fid': 0,
+        'input': None,
+    }
+    ffill = Fulfillment.from_dict(ffill)
+
+    assert ffill.to_dict() == expected.to_dict()
+
+
+def test_fulfillment_deserialization_with_dict(cc_ffill, user_vk):
+    from bigchaindb.transaction import Fulfillment
+
+    expected = Fulfillment(cc_ffill, [user_vk])
+
+    ffill = {
+        'owners_before': [user_vk],
+        'fulfillment': None,
+        'details': cc_ffill.to_dict(),
+        'fid': 0,
+        'input': None,
+    }
+    ffill = Fulfillment.from_dict(ffill)
+
+    assert ffill.to_dict() == expected.to_dict()
+
+
+def test_invalid_fulfillment_initialization(cc_ffill, user_vk):
+    from bigchaindb.transaction import Fulfillment
+
     with raises(TypeError):
-        Fulfillment(ffill, user_vk)
+        Fulfillment(cc_ffill, user_vk)
 
 
-def test_condition_serialization(user_vk):
-    from bigchaindb.transaction import (
-        Condition,
-    )
+def test_gen_default_fulfillment(user_vk):
+    from bigchaindb.transaction import Fulfillment
+    from cryptoconditions import Ed25519Fulfillment
 
-    cond = Condition.gen_default(user_vk)
-    cond_dict = cond.to_dict()
-    assert cond_dict['owners_after'] == [user_vk]
-    assert cond_dict['condition']['uri'] == cond.condition.condition_uri
-    assert cond_dict['cid'] == 0
+    ffill = Fulfillment.gen_default(user_vk)
+    assert ffill.owners_before == [user_vk]
+    assert ffill.fid == 0
+    assert ffill.fulfillment.to_dict() == Ed25519Fulfillment(public_key=user_vk).to_dict()
+    assert ffill.tx_input is None
 
 
-def test_condition_deserialization(user_vk):
-    from bigchaindb.transaction import (
-        Condition,
-    )
+def test_condition_serialization(cond_uri, user_vk):
+    from bigchaindb.transaction import Condition
 
-    cond = Condition.gen_default(user_vk)
-    cond_dict = {
-        'condition': {
-            'uri': cond.condition.condition_uri,
-            'details': cond.condition.to_dict()
-        },
+    expected = {
+        'condition': cond_uri,
         'owners_after': [user_vk],
         'cid': 0,
     }
 
-    assert Condition.from_dict(cond_dict).to_dict() == cond_dict
+    cond = Condition(cond_uri, [user_vk])
+
+    assert cond.to_dict() == expected
 
 
-def test_invalid_condition_initialization(ffill, user_vk):
-    from bigchaindb.transaction import (
-        Condition,
-    )
+def test_condition_deserialization(cond_uri, user_vk):
+    from bigchaindb.transaction import Condition
+
+    expected = Condition(cond_uri, [user_vk])
+    cond = {
+        'condition': cond_uri,
+        'owners_after': [user_vk],
+        'cid': 0,
+    }
+    cond = Condition.from_dict(cond)
+
+    assert cond.to_dict() == expected.to_dict()
+
+
+def test_invalid_condition_initialization(cond_uri, user_vk):
+    from bigchaindb.transaction import Condition
+
     with raises(TypeError):
-        Condition(ffill.condition, user_vk)
+        Condition(cond_uri, user_vk)
 
 
-def test_gen_default_condition(user_vk):
-    from bigchaindb.transaction import (
-        Condition,
-    )
-    from cryptoconditions import Ed25519Fulfillment
-    cond = Condition.gen_default(user_vk)
-    assert cond.owners_after == [user_vk]
-    assert cond.cid == 0
-    # TODO: Would be nice if Cryptoconditions would implement a `__eq__` method
-    # NOTE: This doesn't make sense yet...
-    assert cond.condition.to_dict() == Ed25519Fulfillment(public_key=user_vk).to_dict()
+def test_invalid_data_initialization():
+    from bigchaindb.transaction import Data
+
+    with raises(TypeError):
+        Data([])
 
 
-def test_create_default_transaction(user_vk):
-    from bigchaindb.transaction import (
-        Condition,
-        Transaction,
-    )
-    cond = Condition.gen_default(user_vk)
-    tx = Transaction([cond])
-    assert tx.conditions == [cond]
-    assert tx.fulfillments == []
-    assert tx.inputs is None
-    assert tx.payload is None
+def test_data_serialization(payload, payload_id):
+    from bigchaindb.transaction import Data
+
+    expected = {
+        'payload': payload,
+        'hash': payload_id
+    }
+    data = Data(payload, payload_id)
+
+    assert data.to_dict() == expected
 
 
-def test_sign_default_transaction(user_vk, user_sk):
-    from bigchaindb.transaction import (
-        Fulfillment,
-        Transaction,
-    )
+def test_data_deserialization(payload, payload_id):
+    from bigchaindb.transaction import Data
 
-    ffill = Fulfillment.gen_default(user_vk)
-    ffill.to_dict()
-    cond = ffill.gen_condition()
-    tx = Transaction(Transaction.CREATE, [ffill], [cond])
-    tx.sign([user_sk])
-    tx_dict = tx.to_dict()
-    # TODO: We need to make sure to serialize the transaction correctly!!!
-    assert len(tx.fulfillments) > 0
-    assert tx_dict['transaction']['conditions'][0] == cond.to_dict()
+    expected = Data(payload, payload_id)
+    data = Data.from_dict({'payload': payload, 'hash': payload_id})
+
+    assert data.to_dict() == expected.to_dict()
+
+
+def test_transaction_serialization(default_ffill, default_cond):
+    from bigchaindb.transaction import Transaction
+
+    tx_id = 'l0l'
+    timestamp = '66666666666'
+
+    expected = {
+        'id': tx_id,
+        'version': Transaction.VERSION,
+        'transaction': {
+            # NOTE: This test assumes that Fulfillments and Conditions can successfully be serialized
+            'fulfillments': [default_ffill.to_dict()],
+            'conditions': [default_cond.to_dict()],
+            'operation': Transaction.CREATE,
+            'timestamp': timestamp,
+            'data': None,
+        }
+    }
+
+    tx_dict = Transaction(Transaction.CREATE, [default_ffill], [default_cond]).to_dict()
+    tx_dict['id'] = tx_id
+    tx_dict['transaction']['timestamp'] = timestamp
+
+    assert tx_dict == expected
+
+
+def test_transaction_deserialization(default_ffill, default_cond):
+    from bigchaindb.transaction import Transaction
+
+    tx_id = 'l0l'
+    timestamp = '66666666666'
+
+    expected = Transaction(Transaction.CREATE, [default_ffill], [default_cond], None, timestamp, Transaction.VERSION)
+
+    tx = {
+        'id': tx_id,
+        'version': Transaction.VERSION,
+        'transaction': {
+            # NOTE: This test assumes that Fulfillments and Conditions can successfully be serialized
+            'fulfillments': [default_ffill.to_dict()],
+            'conditions': [default_cond.to_dict()],
+            'operation': Transaction.CREATE,
+            'timestamp': timestamp,
+            'data': None,
+        }
+    }
+    tx = Transaction.from_dict(tx)
+
+    assert tx.to_dict() == expected.to_dict()
+
+
+def test_invalid_tx_initialization():
+    from bigchaindb.transaction import Transaction
+
+    wrong_data_type = {'payload': 'a totally wrong datatype'}
+    with raises(TypeError):
+        Transaction(Transaction.CREATE, wrong_data_type)
+    with raises(TypeError):
+        Transaction(Transaction.CREATE, [], wrong_data_type)
+    with raises(TypeError):
+        Transaction(Transaction.CREATE, [], [], wrong_data_type)
