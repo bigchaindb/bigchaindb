d222a848bc929fd66ea109ded733f1efad675d46
troymc <troy@ascribe.io>
2016-08-15 19:53:44 +0200

    Added mapping of aws_region -> recent Ubuntu AMIs

---
+++ ntools/terraform/aws1/amis.tf
@@ -0,0 +1,16 @@
+# These are all Ubuntu 14.04 LTS AMIs
+# with Arch = amd64, Instance Type = hvm:ebs-ssd
+# from https://cloud-images.ubuntu.com/locator/ec2/
+variable "amis" {
+  type = "map"
+  default = {
+    eu-west-1      = "ami-55452e26"
+    eu-central-1   = "ami-b1cf39de"
+    us-east-1      = "ami-8e0b9499"
+    us-west-2      = "ami-547b3834"
+    ap-northeast-1 = "ami-49d31328"
+    ap-southeast-1 = "ami-5e429c3d"
+    ap-southeast-2 = "ami-25f3c746"
+    sa-east-1      = "ami-97980efb"
+  }
+}
