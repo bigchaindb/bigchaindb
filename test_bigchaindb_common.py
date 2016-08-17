0c228b3c6f99644a3e4b03a061c428535f428772
Sylvain Bellemare <sylvain@ascribe.io>
2016-08-17 12:00:16 +0200

    Planning release

---
+++ test_bigchaindb_common.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_bigchaindb_common
+----------------------------------
+
+Tests for `bigchaindb_common` module.
+"""
+
+import pytest
+
+
+from bigchaindb_common import bigchaindb_common
+
+
+class TestBigchaindb_common(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
