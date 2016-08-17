71700ec51f37b3ac4a6bcd4fbaa1274f82d12162
tim <tim@ascribe.io>
2016-08-17 15:05:16 +0200

    Adjust setup to package structure

---
+++ util.py
@@ -0,0 +1,33 @@
+import time
+
+import rapidjson
+
+
+def gen_timestamp():
+    """The Unix time, rounded to the nearest second.
+        See https://en.wikipedia.org/wiki/Unix_time
+
+        Returns:
+    str: the Unix time
+    """
+    return str(round(time.time()))
+
+
+def serialize(data):
+    """Serialize a dict into a JSON formatted string.
+
+        This function enforces rules like the separator and order of keys. This ensures that all dicts
+        are serialized in the same way.
+
+        This is specially important for hashing data. We need to make sure that everyone serializes their data
+        in the same way so that we do not have hash mismatches for the same structure due to serialization
+        differences.
+
+        Args:
+        data (dict): dict to serialize
+
+                     Returns:
+        str: JSON formatted string
+
+    """
+    return rapidjson.dumps(data, skipkeys=False, ensure_ascii=False, sort_keys=True)
