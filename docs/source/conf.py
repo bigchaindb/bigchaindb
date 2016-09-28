e74b4ee5288267147ddb34fcb8342a069b5d198a
Sylvain Bellemare <sbellem@gmail.com>
2016-09-28 16:58:30 +0200

    Link docs to python 3 (#647)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -309,4 +309,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3': None}
