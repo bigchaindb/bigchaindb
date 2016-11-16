2f6e8abac22fd387a2d455a8ff21f63c94d6b3be
troymc <troy@ascribe.io>
2016-11-16 10:10:09 +0100

    Fixed the call to Transaction.create(): 2nd arg a list of tuples

--- bigchaindb/commands/bigchain.py
+++ bigchaindb/commands/bigchain.py
@@ -193,7 +193,7 @@ def _run_load(tx_left, stats):
     b = bigchaindb.Bigchain()
 
     while True:
-        tx = Transaction.create([b.me], [b.me])
+        tx = Transaction.create([b.me], [([b.me], 1)])
         tx = tx.sign([b.me_private])
         b.write_transaction(tx)
 
