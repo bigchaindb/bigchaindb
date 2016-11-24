0d8a53cb68400ba5f68c747b4c37b2b1eacf64ca
Scott Sadler <scott@alphanode.co>
2016-11-24 13:54:09 +0100

    vote schema

--- bigchaindb/common/schema/__init__.py
+++ bigchaindb/common/schema/__init__.py
@@ -7,18 +7,31 @@ import yaml
 from bigchaindb.common.exceptions import SchemaValidationError
 
 
-TX_SCHEMA_PATH = os.path.join(os.path.dirname(__file__), 'transaction.yaml')
-with open(TX_SCHEMA_PATH) as handle:
-    TX_SCHEMA_YAML = handle.read()
-TX_SCHEMA = yaml.safe_load(TX_SCHEMA_YAML)
+def _load_schema(name):
+    """ Load a schema from disk """
+    path = os.path.join(os.path.dirname(__file__), name + '.yaml')
+    with open(path) as handle:
+        return path, yaml.safe_load(handle)
 
 
-def validate_transaction_schema(tx_body):
-    """ Validate a transaction dict against a schema """
+def _validate_schema(schema, body):
+    """ Validate data against a schema """
     try:
-        jsonschema.validate(tx_body, TX_SCHEMA)
+        jsonschema.validate(body, schema)
     except jsonschema.ValidationError as exc:
         raise SchemaValidationError(str(exc)) from exc
 
 
-__all__ = ['TX_SCHEMA', 'TX_SCHEMA_YAML', 'validate_transaction_schema']
+TX_SCHEMA_PATH, TX_SCHEMA = _load_schema('transaction')
+VOTE_SCHEMA_PATH, VOTE_SCHEMA = _load_schema('vote')
+
+
+def validate_transaction_schema(tx_body):
+    """ Validate a transaction dict """
+    _validate_schema(TX_SCHEMA, tx_body)
+
+
+def validate_vote_schema(tx_body):
+    """ Validate a vote dict """
+    _validate_schema(VOTE_SCHEMA, tx_body)
+
