49726c4a31fbe6d0d9308aa968befffc15a8872e
Scott Sadler <scott@alphanode.co>
2016-11-30 16:20:17 +0100

    `make html` runs generation scripts for documentation, generates http server examples

--- docs/server/source/conf.py
+++ docs/server/source/conf.py
@@ -35,6 +35,10 @@ _version = {}
 with open('../../../bigchaindb/version.py') as fp:
     exec(fp.read(), _version)
 
+import os.path
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/..'))
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -44,6 +48,10 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinxcontrib.httpdomain',
     'sphinx.ext.autosectionlabel',
+    # Below are actually build steps made to look like sphinx extensions.
+    # It was the easiest way to get it running with ReadTheDocs.
+    'generate_schema_documentation',
+    'generate_http_server_api_documentation',
 ]
 
 # autodoc settings
