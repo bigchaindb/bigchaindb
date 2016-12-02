c9a3d011129a4722aff285c507ff44eca52d8cf5
Brett Sun <qisheng.brett.sun@gmail.com>
2016-12-02 14:24:40 +0100

    Move db/ files into backend/

--- bigchaindb/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from bigchaindb.db.factory import get_backend_factory
-from bigchaindb.db.query import Query
-from bigchaindb.db.schema import Schema
-from bigchaindb.db.connection import Connection
