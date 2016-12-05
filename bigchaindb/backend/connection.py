e73164e8d55e9f0234ab311307e3a1f962396950
vrde <agranzot@gmail.com>
2016-12-05 14:09:40 +0100

    Add error handling

--- bigchaindb/backend/connection.py
+++ bigchaindb/backend/connection.py
@@ -1,4 +1,6 @@
+from bigchaindb.common.exceptions import ConfigurationError
 from importlib import import_module
+
 
 BACKENDS = {
     'rethinkdb': 'bigchaindb.backend.rethinkdb.connection.RethinkDBConnection'
@@ -6,8 +8,15 @@ BACKENDS = {
 
 
 def connect(backend, host, port, dbname):
-    module_name, _, class_name = BACKENDS[backend].rpartition('.')
-    Class = getattr(import_module(module_name), class_name)
+    try:
+        module_name, _, class_name = BACKENDS[backend].rpartition('.')
+        Class = getattr(import_module(module_name), class_name)
+    except KeyError:
+        raise ConfigurationError('Backend `{}` is not supported. '
+                                 'BigchainDB currently supports {}'.format(backend, BACKENDS.keys()))
+    except (ImportError, AttributeError) as exc:
+        raise ConfigurationError('Error loading backend `{}`'.format(backend)) from exc
+
     return Class(host, port, dbname)
 
 
