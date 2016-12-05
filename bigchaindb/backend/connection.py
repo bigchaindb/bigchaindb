5b57ea138f123282f7f50b875455a1fcf4c84f17
vrde <agranzot@gmail.com>
2016-12-05 14:41:22 +0100

    Add docstring, fix name

--- bigchaindb/backend/connection.py
+++ bigchaindb/backend/connection.py
@@ -8,10 +8,23 @@ BACKENDS = {
 }
 
 
-def connect(backend, host=None, port=None, name=None):
+def connect(backend=None, host=None, port=None, name=None):
+    '''Create a connection to a database.
+
+    Args:
+        backend (str): the name of the backend to use.
+        host (str): the host to connect to.
+        port (int): the port to connect to.
+        name (str): the name of the database to use.
+
+    Returns:
+        An instance of :class:`~bigchaindb.backend.connection.Connection`.
+    '''
+
+    backend = backend or bigchaindb.config['database']['backend']
     host = host or bigchaindb.config['database']['host']
     port = port or bigchaindb.config['database']['port']
-    name = name or bigchaindb.config['database']['name']
+    dbname = name or bigchaindb.config['database']['name']
 
     try:
         module_name, _, class_name = BACKENDS[backend].rpartition('.')
@@ -22,7 +35,7 @@ def connect(backend, host=None, port=None, name=None):
     except (ImportError, AttributeError) as exc:
         raise ConfigurationError('Error loading backend `{}`'.format(backend)) from exc
 
-    return Class(host, port, name)
+    return Class(host, port, dbname)
 
 
 class Connection:
