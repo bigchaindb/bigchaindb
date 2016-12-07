8a06469c293b6e0d924d8ce257cf3140b609ea19
Brett Sun <qisheng.brett.sun@gmail.com>
2016-12-07 15:29:08 +0100

    Rename  to  in schema function arguments to for consistency

--- bigchaindb/backend/schema.py
+++ bigchaindb/backend/schema.py
@@ -7,78 +7,79 @@ from bigchaindb.backend.connection import connect
 
 
 @singledispatch
-def create_database(connection, name):
+def create_database(connection, dbname):
     """Create database to be used by BigchainDB
 
     Args:
-        name (str): the name of the database to create.
+        dbname (str): the name of the database to create.
 
     Raises:
         :exc:`~bigchaindb.common.exceptions.DatabaseAlreadyExists`: If the
-        given :attr:`name` already exists as a database.
+        given :attr:`dbname` already exists as a database.
     """
 
     raise NotImplementedError
 
 
 @singledispatch
-def create_tables(connection, name):
+def create_tables(connection, dbname):
     """Create the tables to be used by BigchainDB
 
     Args:
-        name (str): the name of the database to create tables for.
+        dbname (str): the name of the database to create tables for.
     """
 
     raise NotImplementedError
 
 
 @singledispatch
-def create_indexes(connection, name):
+def create_indexes(connection, dbname):
     """Create the indexes to be used by BigchainDB
 
     Args:
-        name (str): the name of the database to create indexes for.
+        dbname (str): the name of the database to create indexes for.
     """
 
     raise NotImplementedError
 
 
 @singledispatch
-def drop_database(connection, name):
+def drop_database(connection, dbname):
     """Drop the database used by BigchainDB
 
     Args:
-        name (str): the name of the database to drop.
+        dbname (str): the name of the database to drop.
 
     Raises:
         :exc:`~bigchaindb.common.exceptions.DatabaseDoesNotExist`: If the
-        given :attr:`name` does not exist as a database.
+        given :attr:`dbname` does not exist as a database.
     """
 
     raise NotImplementedError
 
 
-def init_database(conn=None, name=None):
+def init_database(conn=None, dbname=None):
     """Initialize the configured backend for use with BigchainDB.
 
-    Creates a database with :attr:`name` with any required tables
+    Creates a database with :attr:`dbname` with any required tables
     and supporting indexes.
 
     Args:
         conn (:class:`~bigchaindb.backend.connection.Connection`): an existing
             connection to use to initialize the database.
             Creates one if not given.
-        name (str): the name of the database to create.
-            Defaults to the name in the BigchainDB configuration.
+        dbname (str): the name of the database to create.
+            Defaults to the database name given in the BigchainDB
+            configuration.
 
     Raises:
         :exc:`~bigchaindb.common.exceptions.DatabaseAlreadyExists`: If the
-        given :attr:`name` already exists as a database.
+        given :attr:`dbname` already exists as a database.
     """
 
     conn = conn or connect()
-    name = name or bigchaindb.config['database']['name']
+    dbname = dbname or bigchaindb.config['database']['name']
 
-    create_database(conn, name)
-    create_tables(conn, name)
-    create_indexes(conn, name)
+    create_database(conn, dbname)
+    create_tables(conn, dbname)
+    create_indexes(conn, dbname)
