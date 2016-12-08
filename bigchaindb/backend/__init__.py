2ee8d78606a7ab3d55609975c3eb42cbd30ea2b2
Brett Sun <qisheng.brett.sun@gmail.com>
2016-12-08 16:43:59 +0100

    Add package-level docstrings to backends

--- bigchaindb/backend/__init__.py
+++ bigchaindb/backend/__init__.py
@@ -1,4 +1,10 @@
-"""Backend interfaces ..."""
+"""Generic backend database interfaces expected by BigchainDB.
+
+The interfaces in this module allow BigchainDB to be agnostic about its
+database backend. One can configure BigchainDB to use different databases as
+its data store by setting the ``database.backend`` property in the
+configuration or the ``BIGCHAINDB_DATABASE_BACKEND`` environment variable.
+"""
 
 # Include the backend interfaces
 from bigchaindb.backend import changefeed, schema, query  # noqa
