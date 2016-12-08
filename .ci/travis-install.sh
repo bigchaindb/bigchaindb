c37d959c67a0aeb4eb0755312d53454a45b7280b
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-08 18:52:06 +0100

    Add mongo setup to travis

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -5,7 +5,13 @@ set -e -x
 pip install --upgrade pip
 pip install --upgrade tox
 
-if [ "${TOXENV}" == "py34" ] || [ "${TOXENV}" == "py35" ]; then
-    sudo apt-get install rethinkdb
+if [ "${TOXENV}" == py* ]; then
+
+    if [ "${TOXENV}" == *-rdb ]; then
+	sudo apt-get install rethinkdb
+    elif [ "${TOXENV}" == *-mdb ]; then
+	sudo apt-get install mongodb-org-server
+    fi
+
     pip install --upgrade codecov
 fi
