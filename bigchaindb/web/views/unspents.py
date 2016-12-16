2b9a35191291aabd3619a211b9df7f3841144068
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-16 22:06:27 +0100

    Rename owner_after to public_key

--- bigchaindb/web/views/unspents.py
+++ bigchaindb/web/views/unspents.py
@@ -15,14 +15,14 @@ class UnspentListApi(Resource):
                 A :obj:`list` of :cls:`str` of links to unfulfilled conditions.
         """
         parser = reqparse.RequestParser()
-        parser.add_argument('owner_after', type=str, location='args',
+        parser.add_argument('public_key', type=str, location='args',
                             required=True)
         args = parser.parse_args()
 
         pool = current_app.config['bigchain_pool']
 
         with pool() as bigchain:
-            unspents = bigchain.get_owned_ids(args['owner_after'])
+            unspents = bigchain.get_owned_ids(args['public_key'])
             # NOTE: We pass '..' as a path to create a valid relative URI
             return [u.to_uri('..') for u in unspents]
 
