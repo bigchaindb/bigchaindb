f5e81a239f29e64f0407419c58b482ae23be1c5e
Sylvain Bellemare <sylvain@ascribe.io>
2016-12-16 22:06:27 +0100

    Rename owner_after to public_key

--- tests/web/test_unspents.py
+++ tests/web/test_unspents.py
@@ -7,20 +7,20 @@ UNSPENTS_ENDPOINT = '/api/v1/unspents/'
 @pytest.mark.usefixtures('inputs')
 def test_get_unspents_endpoint(b, client, user_pk):
     expected = [u.to_uri('..') for u in b.get_owned_ids(user_pk)]
-    res = client.get(UNSPENTS_ENDPOINT + '?owner_after={}'.format(user_pk))
+    res = client.get(UNSPENTS_ENDPOINT + '?public_key={}'.format(user_pk))
     assert expected == res.json
     assert res.status_code == 200
 
 
 @pytest.mark.usefixtures('inputs')
-def test_get_unspents_endpoint_without_owner_after(client):
+def test_get_unspents_endpoint_without_public_key(client):
     res = client.get(UNSPENTS_ENDPOINT)
     assert res.status_code == 400
 
 
 @pytest.mark.usefixtures('inputs')
-def test_get_unspents_endpoint_with_unused_owner_after(client):
+def test_get_unspents_endpoint_with_unused_public_key(client):
     expected = []
-    res = client.get(UNSPENTS_ENDPOINT + '?owner_after=abc')
+    res = client.get(UNSPENTS_ENDPOINT + '?public_key=abc')
     assert expected == res.json
     assert res.status_code == 200
