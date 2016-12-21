4a14c7c8f10577b0fbd31ca1ca1dde8e3d0ba2ab
Rodolphe Marques <marques.rodolphe@gmail.com>
2016-12-21 15:53:25 +0100

    Initialize replica set for mongodb when running bigchaindb init

--- tests/backend/mongodb/test_schema.py
+++ tests/backend/mongodb/test_schema.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import patch
 
 
 @pytest.mark.usefixtures('setup_database')
@@ -107,3 +108,37 @@ def test_drop():
 
     schema.drop_database(conn, dbname)
     assert dbname not in conn.conn.database_names()
+
+
+@pytest.mark.usefixtures('setup_database')
+def test_get_replica_set_name():
+    from pymongo.database import Database
+    from bigchaindb import backend
+    from bigchaindb.backend.mongodb.schema import _get_replica_set_name
+    from bigchaindb.common.exceptions import ConfigurationError
+
+    conn = backend.connect()
+
+    # no replSet option set
+    cmd_line_opts = {'argv': ['mongod', '--dbpath=/data'],
+                     'ok': 1.0,
+                     'parsed': {'storage': {'dbPath': '/data'}}}
+    with patch.object(Database, 'command', return_value=cmd_line_opts):
+        with pytest.raises(ConfigurationError):
+            _get_replica_set_name(conn)
+
+    # replSet option set through the command line
+    cmd_line_opts = {'argv': ['mongod', '--dbpath=/data', '--replSet=rs0'],
+                     'ok': 1.0,
+                     'parsed': {'replication': {'replSet': 'rs0'},
+                                'storage': {'dbPath': '/data'}}}
+    with patch.object(Database, 'command', return_value=cmd_line_opts):
+        assert _get_replica_set_name(conn) == 'rs0'
+
+    # replSet option set through the config file
+    cmd_line_opts = {'argv': ['mongod', '--dbpath=/data', '--replSet=rs0'],
+                     'ok': 1.0,
+                     'parsed': {'replication': {'replSetName': 'rs0'},
+                                'storage': {'dbPath': '/data'}}}
+    with patch.object(Database, 'command', return_value=cmd_line_opts):
+        assert _get_replica_set_name(conn) == 'rs0'
