64d12865cc261ba56704723eec459638f4b6a0d4
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-06 16:21:01 +0100

    travis setup take 3

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -5,7 +5,10 @@ set -e -x
 if [[ "${TOXENV}" == *-rdb ]]; then
     rethinkdb --daemon
 elif [[ "${TOXENV}" == *-mdb ]]; then
-    echo "replSet = rs0" | sudo tee -a /etc/mongodb.conf
+    sudo tee -a /etc/mongod.conf <<EOF
+    replication:
+      replSetName: bigchain
+    EOF
     sudo service mongod restart
     sleep 15
 fi
