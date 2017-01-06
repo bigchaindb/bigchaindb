7f4d45ae3256481b4524af8147242f63a3a4dd88
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-06 15:49:58 +0100

    add travis support for replica set

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -5,5 +5,6 @@ set -e -x
 if [[ "${TOXENV}" == *-rdb ]]; then
     rethinkdb --daemon
 elif [[ "${TOXENV}" == *-mdb ]]; then
+    echo "replSet = rs0" | sudo tee -a
     sudo service mongod start
 fi
