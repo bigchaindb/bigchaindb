d75f35b434f2f2bcec0bdbd1f34e070d5ce1dc17
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-06 18:23:16 +0100

    travis setup take 10

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -6,8 +6,8 @@ if [[ "${TOXENV}" == *-rdb ]]; then
     rethinkdb --daemon
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
     echo 'Setting up mongodb'
-    wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-ubuntu1404-3.4.1.tgz -O /tmp/mongodb.tgz
+    wget http://downloads.mongodb.org/linux/mongodb-linux-x86_64-3.4.1.tgz -O /tmp/mongodb.tgz
     tar -xvf /tmp/mongodb.tgz
     mkdir /tmp/data
-    ${PWD}/mongodb-linux-x86_64-enterprise-ubuntu1404-3.4.1/bin/mongod --dbpath /tmp/data --replSet=rs0 &> /dev/null &
+    ${PWD}/mongodb-linux-x86_64-3.4.1/bin/mongod --dbpath /tmp/data --replSet=rs0 &> /dev/null &
 fi
