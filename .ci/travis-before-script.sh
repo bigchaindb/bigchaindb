b60accbe46b7481415804c46b407099e10136a85
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-06 15:57:09 +0100

    fix travis setup

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -6,5 +6,6 @@ if [[ "${TOXENV}" == *-rdb ]]; then
     rethinkdb --daemon
 elif [[ "${TOXENV}" == *-mdb ]]; then
     echo "replSet = rs0" | sudo tee -a
-    sudo service mongod start
+    sudo service mongod restart
+    sleep 15
 fi
