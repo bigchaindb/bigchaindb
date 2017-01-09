9bc658b83387115bb97821604154bfbdbe68193f
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-09 14:03:57 +0100

    fix travis mongodb integration

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -4,6 +4,8 @@ set -e -x
 
 if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
+elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
+    pytest -vs --database-backend=mongodb --cov=bigchaindb
 else
   pytest -v -n auto --cov=bigchaindb
 fi
