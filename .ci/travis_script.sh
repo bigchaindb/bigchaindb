abf77c04eea86b183890838b07b55af2c5486475
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-09 14:10:49 +0100

    Omit mongod log messages.
    MongoDB failures no longer allowed

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -5,7 +5,7 @@ set -e -x
 if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
-    pytest -vs --database-backend=mongodb --cov=bigchaindb
+    pytest -v --database-backend=mongodb --cov=bigchaindb
 else
   pytest -v -n auto --cov=bigchaindb
 fi
