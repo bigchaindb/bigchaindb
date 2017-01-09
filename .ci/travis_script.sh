a0b2ae3672ff7b8c9d000a6630d3535da2a54197
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-09 09:44:00 +0100

    travis setup

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -5,7 +5,7 @@ set -e -x
 if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
-    pytest -v --cov=bigchaindb
+    pytest -vs --cov=bigchaindb
 else
   pytest -v -n auto --cov=bigchaindb
 fi
