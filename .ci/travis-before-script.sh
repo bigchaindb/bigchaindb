35d9d6069d73ffa4023712de8cff74a1813613ee
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-01-09 10:16:23 +0100

    travis setup

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -8,6 +8,6 @@ elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
     echo 'Setting up mongodb'
     wget http://downloads.mongodb.org/linux/mongodb-linux-x86_64-3.4.1.tgz -O /tmp/mongodb.tgz
     tar -xvf /tmp/mongodb.tgz
-    mkdir /tmp/data
-    ${PWD}/mongodb-linux-x86_64-3.4.1/bin/mongod --dbpath /tmp/data --replSet=rs0 &
+    mkdir mongodb-data
+    ${PWD}/mongodb-linux-x86_64-3.4.1/bin/mongod --dbpath=${PWD}/mongodb-data --replSet=rs0 &
 fi
