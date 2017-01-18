52450e22acc8715d8d7441f639c7df1b0d6932d5
Sylvain Bellemare <sylvain@ascribe.io>
2017-01-18 14:18:09 +0100

    Use url_for to get the path to test

--- tests/web/test_statuses.py
+++ tests/web/test_statuses.py
@@ -1,29 +1,36 @@
 import pytest
+from flask import url_for
 
 from bigchaindb.models import Transaction
-
-STATUSES_ENDPOINT = '/api/v1/statuses'
 
 
 @pytest.mark.bdb
 @pytest.mark.usefixtures('inputs')
 def test_get_transaction_status_endpoint(b, client, user_pk):
+    from bigchaindb.web.views.statuses import StatusApi
+    from bigchaindb.web.views.transactions import TransactionApi
     input_tx = b.get_owned_ids(user_pk).pop()
     tx, status = b.get_transaction(input_tx.txid, include_status=True)
-    res = client.get(STATUSES_ENDPOINT + '?tx_id=' + input_tx.txid)
+    path = url_for(StatusApi.__name__.lower(), tx_id=input_tx.txid)
+    res = client.get(path=path)
     assert status == res.json['status']
-    assert res.json['_links']['tx'] == '/transactions/{}'.format(input_tx.txid)
+    expected_tx_link = url_for(
+        TransactionApi.__name__.lower(), tx_id=input_tx.txid)[7:]
+    assert res.json['_links']['tx'] == expected_tx_link
     assert res.status_code == 200
 
 
 @pytest.mark.bdb
 def test_get_transaction_status_endpoint_returns_404_if_not_found(client):
-    res = client.get(STATUSES_ENDPOINT + '?tx_id=123')
+    from bigchaindb.web.views.statuses import StatusApi
+    path = url_for(StatusApi.__name__.lower(), tx_id=123)
+    res = client.get(path=path)
     assert res.status_code == 404
 
 
 @pytest.mark.bdb
 def test_get_block_status_endpoint_undecided(b, client):
+    from bigchaindb.web.views.statuses import StatusApi
     tx = Transaction.create([b.me], [([b.me], 1)])
     tx = tx.sign([b.me_private])
 
@@ -32,7 +39,8 @@ def test_get_block_status_endpoint_undecided(b, client):
 
     status = b.block_election_status(block.id, block.voters)
 
-    res = client.get(STATUSES_ENDPOINT + '?block_id=' + block.id)
+    path = url_for(StatusApi.__name__.lower(), block_id=block.id)
+    res = client.get(path=path)
     assert status == res.json['status']
     assert '_links' not in res.json
     assert res.status_code == 200
@@ -41,6 +49,7 @@ def test_get_block_status_endpoint_undecided(b, client):
 @pytest.mark.bdb
 @pytest.mark.usefixtures('inputs')
 def test_get_block_status_endpoint_valid(b, client):
+    from bigchaindb.web.views.statuses import StatusApi
     tx = Transaction.create([b.me], [([b.me], 1)])
     tx = tx.sign([b.me_private])
 
@@ -53,7 +62,8 @@ def test_get_block_status_endpoint_valid(b, client):
 
     status = b.block_election_status(block.id, block.voters)
 
-    res = client.get(STATUSES_ENDPOINT + '?block_id=' + block.id)
+    path = url_for(StatusApi.__name__.lower(), block_id=block.id)
+    res = client.get(path=path)
     assert status == res.json['status']
     assert '_links' not in res.json
     assert res.status_code == 200
@@ -62,6 +72,7 @@ def test_get_block_status_endpoint_valid(b, client):
 @pytest.mark.bdb
 @pytest.mark.usefixtures('inputs')
 def test_get_block_status_endpoint_invalid(b, client):
+    from bigchaindb.web.views.statuses import StatusApi
     tx = Transaction.create([b.me], [([b.me], 1)])
     tx = tx.sign([b.me_private])
 
@@ -74,7 +85,8 @@ def test_get_block_status_endpoint_invalid(b, client):
 
     status = b.block_election_status(block.id, block.voters)
 
-    res = client.get(STATUSES_ENDPOINT + '?block_id=' + block.id)
+    path = url_for(StatusApi.__name__.lower(), block_id=block.id)
+    res = client.get(path=path)
     assert status == res.json['status']
     assert '_links' not in res.json
     assert res.status_code == 200
@@ -82,17 +94,19 @@ def test_get_block_status_endpoint_invalid(b, client):
 
 @pytest.mark.bdb
 def test_get_block_status_endpoint_returns_404_if_not_found(client):
-    res = client.get(STATUSES_ENDPOINT + '?block_id=123')
+    from bigchaindb.web.views.statuses import StatusApi
+    path = url_for(StatusApi.__name__.lower(), block_id=123)
+    res = client.get(path=path)
     assert res.status_code == 404
 
 
 @pytest.mark.bdb
-def test_get_status_endpoint_returns_400_bad_query_params(client):
-    res = client.get(STATUSES_ENDPOINT)
-    assert res.status_code == 400
-
-    res = client.get(STATUSES_ENDPOINT + '?ts_id=123')
-    assert res.status_code == 400
-
-    res = client.get(STATUSES_ENDPOINT + '?tx_id=123&block_id=123')
+@pytest.mark.parametrize('query_params', (
+    {}, {'ts_id': 123}, {'tx_id': 123, 'block_id': 123}
+))
+def test_get_status_endpoint_returns_400_bad_query_params(client,
+                                                          query_params):
+    from bigchaindb.web.views.statuses import StatusApi
+    path = url_for(StatusApi.__name__.lower(), **query_params)
+    res = client.get(path)
     assert res.status_code == 400
