1588681c5b7edabf9e6b02206053a557e3233c73
vrde <agranzot@gmail.com>
2017-01-31 17:13:26 +0100

    Fix pep8 error

--- bigchaindb/backend/exceptions.py
+++ bigchaindb/backend/exceptions.py
@@ -4,5 +4,6 @@ from bigchaindb.exceptions import BigchainDBError
 class ConnectionError(BigchainDBError):
     """Exception raised when the connection to the DataBase fails."""
 
+
 class DatabaseOpFailedError(BigchainDBError):
     """Exception for database operation errors."""
