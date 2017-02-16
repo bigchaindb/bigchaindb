aac0c4a0ce84244f35ad980218cf7d886b0be69b
Scott Sadler <scott@alphanode.co>
2017-02-16 16:41:04 +0100

    watchdog double spend across blocks

--- bigchaindb/watchdog.py
+++ bigchaindb/watchdog.py
@@ -1,8 +1,12 @@
 import pymongo
 from bigchaindb.core import Bigchain
 from collections import defaultdict
+import logging as _logging
 import time
 import threading
+
+
+log = _logging.getLogger('watchdog')
 
 
 class Watchdog(threading.Thread):
@@ -10,6 +14,8 @@ class Watchdog(threading.Thread):
         self.b = Bigchain()
         self.sets = defaultdict(set)
         self.blocks = {}
+        self.valid_blocks = set()
+        self.utxos = set()
         self.error = None
         self.ready = False
         self.stop = False
@@ -23,8 +29,7 @@ class Watchdog(threading.Thread):
     def join(self):
         self.stop = True
         super(Watchdog, self).join()
-        if self.error:
-            raise self.error
+        return self.error
 
     def run(self):
         connection = self.b.connection
@@ -42,6 +47,8 @@ class Watchdog(threading.Thread):
 
         self.ready = True
 
+        log.info('Watchdog listening')
+
         while cursor.alive:
             try:
                 self.record = cursor.next()
@@ -53,8 +60,11 @@ class Watchdog(threading.Thread):
             try:
                 self.process(self.record)
             except Exception as e:
+                if type(e) == AssertionError:
+                    e = e.args[0] if len(e.args) == 1 else e.args
                 self.error = e
-                raise
+                log.warn('Watchdog caught: %s', repr(e))
+                break
 
     def process(self, item):
         table = item['ns'].split('.')[1]
@@ -63,44 +73,51 @@ class Watchdog(threading.Thread):
     def process_bigchain(self, item):
         assert item['op'] == 'i', 'BIGCHAIN INSERT ONLY'
         block = item['o']
+        log.info('Process block %s', block['id'])
 
         self.blocks[block['id']] = block
         self.sets['bigchain'].add(block['id'])
 
-        self.check_dupe_tx_in_block(block)
-        self.check_double_spend_in_block(block)
-
     def process_votes(self, item):
-        assert item['op'] == 'i', 'VOTES INSERT ONLY'
         block_id = item['o']['vote']['voting_for_block']
+        log.info('Process vote for block: %s', block_id)
+
+        block_id = item['o']['vote']['voting_for_block']
+        assert item['op'] == 'i', 'VOTES INSERT ONLY'
 
         block = self.blocks[block_id]
 
-        # assert no double vote
-        # assert no status change (valid -> invalid)
-
         status = self.b.block_election_status(block_id,
                                               block['block']['voters'])
-        if status == 'valid':
-            self.sets['valid-blocks'].add(block_id)
-            # TODO: intersections must be null
-            # TODO: dont use self.sets
-            self.sets['valid-tx'].update(self.sets['block-txs-' + block_id])
-            self.sets['txo'].update(self.sets['block-spends-' + block_id])
 
-    def check_double_spend_in_block(self, block):
-        # TODO: This should warn but not error, since it's not part of
-        # valid set
-        spends = set()
-        for tx in block['block']['transactions']:
-            if tx['operation'] == 'TRANSFER':
-                for inp in tx['inputs']:
-                    ff = inp['fulfills']
-                    ff = (ff['txid'], ff['output'])
-                    assert ff not in spends, 'DOUBLE SPEND IN BLOCK'
-                    spends.add(ff)
+        if block_id in self.sets['valid-block']:
+            pass  # ...
+        elif status == 'valid':
+            spends = set(get_block_spends(block))
+            assert spends.difference(self.utxos) == set(), \
+                ('DOUBLE SPEND ACROSS BLOCKS', block_id)
+            self.valid_blocks.add(block_id)
+            self.utxos.difference_update(spends)
+            self.utxos.update(get_block_outputs(block))
 
+
+"""
     def check_dupe_tx_in_block(self, block):
         txids = [tx['id'] for tx in block['block']['transactions']]
         assert len(txids) == len(set(txids)), 'BLOCK DUPLICATE TX'
         self.sets['block-txs-' + block['id']].update(txids)
+"""
+
+
+def get_block_spends(block):
+    for tx in block['block']['transactions']:
+        if tx['operation'] == 'TRANSFER':
+            for inp in tx['inputs']:
+                ff = inp['fulfills']
+                yield (ff['txid'], ff['output'])
+
+
+def get_block_outputs(block):
+    for tx in block['block']['transactions']:
+        for i in range(len(tx['outputs'])):
+            yield (tx['id'], i)
---
+++ tests/pipelines/watchdog/test_watchdog.py
@@ -0,0 +1,59 @@
+import pytest
+import random
+from bigchaindb import watchdog as dog
+
+"""
+
+Correctness assertions
+
+Tx output spent only once (across one block or many blocks)
+Tx appears only once (across one block or many blocks)
+No structurally invalid Tx (schema, policy, sum of inputs == sum of outputs)
+No Tx that spends non existing output
+All fulfillments must contain signature
+All signatures are verified using Tx identity and crypto-condition of output
+Threshold and Ed25519 condition is supported (up to some size limit)
+All Txes verifiable according to signatures and hash(serialize(tx))
+Txes appear in dependency order which is a PARTIAL order
+We are able to provide non embarrassing timestamp
+"""
+
+pytestmark = [pytest.mark.bdb]
+
+
+@pytest.fixture
+def ctx(b, changing_timestamps, genesis_block, watchdog):
+    b.create_genesis_block
+    return type('', (), {'watchdog': watchdog})()
+
+
+def create_tx(b):
+    from bigchaindb.common.transaction import Transaction
+    metadata = {'r': random.random()}
+    return (Transaction.create([b.me], [([b.me], 1)], metadata)
+                       .sign([b.me_private]))
+
+
+def transfer_tx(b, txs):
+    from bigchaindb.common.transaction import Transaction
+    metadata = {'r': random.random()}
+    txin = txs[0]
+    tx = Transaction.transfer(txin.to_inputs(), [([b.me], 1)],
+                              asset_id=txin.id, metadata=metadata)
+    return tx.sign([b.me_private])
+
+
+def write_block(b, txs):
+    block = b.create_block(txs)
+    b.write_block(block)
+    vote = b.vote(block.id, b.get_last_voted_block().id, True)
+    b.write_vote(vote)
+    return block
+
+
+def test_double_spend_across_blocks(b, ctx):
+    tx_create = create_tx(b)
+    write_block(b, [tx_create])
+    write_block(b, [transfer_tx(b, [tx_create])])
+    block = write_block(b, [transfer_tx(b, [tx_create])])
+    assert ctx.watchdog.join() == ('DOUBLE SPEND ACROSS BLOCKS', block.id)
