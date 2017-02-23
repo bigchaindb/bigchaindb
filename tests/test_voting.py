fdad8cd79687fc78f45fead2b748a4cb6cb113c5
Scott Sadler <scott@alphanode.co>
2017-02-23 17:58:08 +0100

    Static Classify Voting

--- tests/test_voting.py
+++ tests/test_voting.py
@@ -1,33 +1,36 @@
 import pytest
+from unittest.mock import patch
 
 from bigchaindb.core import Bigchain
-from bigchaindb.voting import (count_votes, partition_eligible_votes,
-                               decide_votes, INVALID, VALID, UNDECIDED)
+from bigchaindb.voting import Voting, INVALID, VALID, UNDECIDED
 
 
 ################################################################################
 # Tests for checking vote eligibility
 
 
-def test_partition_eligible_votes():
+@patch('bigchaindb.voting.Voting.verify_vote_signature')
+def test_partition_eligible_votes(_):
     nodes = list(map(Bigchain, 'abc'))
     votes = [n.vote('block', 'a', True) for n in nodes]
 
-    el, inel = partition_eligible_votes(votes, 'abc', lambda _: True)
+    el, inel = Voting.partition_eligible_votes(votes, 'abc')
 
     assert el == votes
     assert inel == []
 
 
-def test_count_votes():
+@patch('bigchaindb.voting.Voting.verify_vote_schema')
+def test_count_votes(_):
     nodes = list(map(Bigchain, 'abc'))
+
     votes = [n.vote('block', 'a', True) for n in nodes]
 
-    assert count_votes(votes) == ({
+    assert Voting.count_votes(votes)['counts'] == {
         'n_valid': 3,
         'n_invalid': 0,
         'n_agree_prev_block': 3
-    }, {})
+    }
 
 
 ################################################################################
@@ -53,26 +56,29 @@ DECISION_TESTS = [dict(
 def test_decide_votes_valid(kwargs):
     kwargs = kwargs.copy()
     kwargs['n_invalid'] = 0
-    assert decide_votes(**kwargs) == VALID
+    assert Voting.decide_votes(**kwargs) == VALID
     kwargs['n_agree_prev_block'] -= 1
-    assert decide_votes(**kwargs) == INVALID
+    assert Voting.decide_votes(**kwargs) == INVALID
     kwargs['n_valid'] -= 1
-    assert decide_votes(**kwargs) == UNDECIDED
+    assert Voting.decide_votes(**kwargs) == UNDECIDED
 
 
 @pytest.mark.parametrize('kwargs', DECISION_TESTS)
 def test_decide_votes_invalid(kwargs):
     kwargs = kwargs.copy()
     kwargs['n_valid'] = 0
-    assert decide_votes(**kwargs) == INVALID
+    assert Voting.decide_votes(**kwargs) == INVALID
     kwargs['n_invalid'] -= 1
-    assert decide_votes(**kwargs) == UNDECIDED
+    assert Voting.decide_votes(**kwargs) == UNDECIDED
 
 
 def test_decide_votes_checks_arguments():
     with pytest.raises(ValueError):
-        decide_votes(n_voters=1, n_valid=2, n_invalid=0, n_agree_prev_block=0)
+        Voting.decide_votes(n_voters=1, n_valid=2, n_invalid=0,
+                            n_agree_prev_block=0)
     with pytest.raises(ValueError):
-        decide_votes(n_voters=1, n_valid=0, n_invalid=2, n_agree_prev_block=0)
+        Voting.decide_votes(n_voters=1, n_valid=0, n_invalid=2,
+                            n_agree_prev_block=0)
     with pytest.raises(ValueError):
-        decide_votes(n_voters=1, n_valid=0, n_invalid=0, n_agree_prev_block=2)
+        Voting.decide_votes(n_voters=1, n_valid=0, n_invalid=0,
+                            n_agree_prev_block=2)
