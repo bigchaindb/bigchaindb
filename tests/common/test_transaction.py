4050389df7a1396f9e385afee257a26ecfb8fdcd
Scott Sadler <scott@alphanode.co>
2017-03-13 14:01:29 +0100

    move test_create_tx_no_asset_id

--- tests/common/test_transaction.py
+++ tests/common/test_transaction.py
@@ -964,3 +964,15 @@ def test_validate_version(utx):
     utx.version = '1.0.0'
     with raises(SchemaValidationError):
         validate_transaction_model(utx)
+
+
+def test_create_tx_no_asset_id(b):
+    from bigchaindb.common.exceptions import SchemaValidationError
+    from bigchaindb.models import Transaction
+    tx = Transaction.create([b.me], [([b.me], 1)])
+    # works
+    Transaction.validate_structure(tx.to_dict())
+    # broken
+    tx.asset['id'] = 'b' * 64
+    with raises(SchemaValidationError):
+        Transaction.validate_structure(tx.to_dict())
