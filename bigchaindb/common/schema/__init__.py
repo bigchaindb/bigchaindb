f51b40b6dcc61ab37a1525453cc5376b8a4eea12
Scott Sadler <scott@alphanode.co>
2017-03-14 17:07:18 +0100

    some documentation and nomenclature fixes in common.schema

--- bigchaindb/common/schema/__init__.py
+++ bigchaindb/common/schema/__init__.py
@@ -28,7 +28,7 @@ def _load_schema(name):
     return path, schema
 
 
-TX_SCHEMA_PATH, TX_SCHEMA = _load_schema('transaction')
+TX_SCHEMA_PATH, TX_SCHEMA_COMMON = _load_schema('transaction')
 _, TX_SCHEMA_CREATE = _load_schema('transaction_create')
 _, TX_SCHEMA_TRANSFER = _load_schema('transaction_transfer')
 VOTE_SCHEMA_PATH, VOTE_SCHEMA = _load_schema('vote')
@@ -43,8 +43,13 @@ def _validate_schema(schema, body):
 
 
 def validate_transaction_schema(tx):
-    """ Validate a transaction dict """
-    _validate_schema(TX_SCHEMA, tx)
+    """
+    Validate a transaction dict.
+
+    TX_SCHEMA_COMMON contains properties that are common to all types of
+    transaction. TX_SCHEMA_[TRANSFER|CREATE] add additional constraints on top.
+    """
+    _validate_schema(TX_SCHEMA_COMMON, tx)
     if tx['operation'] == 'TRANSFER':
         _validate_schema(TX_SCHEMA_TRANSFER, tx)
     else:
