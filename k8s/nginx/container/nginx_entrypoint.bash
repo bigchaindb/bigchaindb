beeaffa71961a08497d22b1340cd67ae4b90638e
krish7919 (Krish) <krishnan.k.iyer@gmail.com>
2017-03-16 20:09:56 +0100

    Multiple changes: - Better log format - Intuitive port number usage - README and examples - Addressed a typo in PYTHON_STYLE_GUIDE.md

--- k8s/nginx/container/nginx_entrypoint.bash
+++ k8s/nginx/container/nginx_entrypoint.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# constants
 NGINX_CONF_FILE=/etc/nginx/nginx.conf
 
 # configure the nginx.conf file with env variables
 sed -i "s|MONGODB_FRONTEND_PORT|${MONGODB_FRONTEND_PORT}|g" $NGINX_CONF_FILE
 sed -i "s|MONGODB_BACKEND_HOST|${MONGODB_BACKEND_HOST}|g" $NGINX_CONF_FILE
 sed -i "s|MONGODB_BACKEND_PORT|${MONGODB_BACKEND_PORT}|g" $NGINX_CONF_FILE
-
 sed -i "s|BIGCHAINDB_FRONTEND_PORT|${BIGCHAINDB_FRONTEND_PORT}|g" $NGINX_CONF_FILE
 sed -i "s|BIGCHAINDB_BACKEND_HOST|${BIGCHAINDB_BACKEND_HOST}|g" $NGINX_CONF_FILE
 sed -i "s|BIGCHAINDB_BACKEND_PORT|${BIGCHAINDB_BACKEND_PORT}|g" $NGINX_CONF_FILE
@@ -18,7 +16,7 @@ for host in $hosts; do
   sed -i "s|MONGODB_WHITELIST|allow ${host};\n    MONGODB_WHITELIST|g" $NGINX_CONF_FILE
 done
 
-# remove the MONGODB_WHITELIST marker from template
+# remove the MONGODB_WHITELIST marker string from template
 sed -i "s|MONGODB_WHITELIST||g" $NGINX_CONF_FILE
 
 # start nginx
