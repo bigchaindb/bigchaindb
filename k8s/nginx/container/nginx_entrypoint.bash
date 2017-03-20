c79c79d3e86b9e2557601c589d8952f0bd660cb8
krish7919 (Krish) <krishnan.k.iyer@gmail.com>
2017-03-20 16:25:00 +0100

    'set -euo pipefail' in nginx entrypoint script

--- k8s/nginx/container/nginx_entrypoint.bash
+++ k8s/nginx/container/nginx_entrypoint.bash
@@ -1,13 +1,21 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
+
+mongo_frontend_port=`printenv MONGODB_FRONTEND_PORT`
+mongo_backend_host=`printenv MONGODB_BACKEND_HOST`
+mongo_backend_port=`printenv MONGODB_BACKEND_PORT`
+bdb_frontend_port=`printenv BIGCHAINDB_FRONTEND_PORT`
+bdb_backend_host=`printenv BIGCHAINDB_BACKEND_HOST`
+bdb_backend_port=`printenv BIGCHAINDB_BACKEND_PORT`
+mongo_whitelist=`printenv MONGODB_WHITELIST`
 
 # sanity checks
-if [[ -z "${MONGODB_FRONTEND_PORT}" || \
-    -z "${MONGODB_BACKEND_HOST}" || \
-    -z "${MONGODB_BACKEND_PORT}" || \
-    -z "${BIGCHAINDB_FRONTEND_PORT}" || \
-    -z "${BIGCHAINDB_BACKEND_HOST}" || \
-    -z "${BIGCHAINDB_BACKEND_PORT}" ]] ; then
+if [[ -z "${mongo_frontend_port}" || \
+    -z "${mongo_backend_host}" || \
+    -z "${mongo_backend_port}" || \
+    -z "${bdb_frontend_port}" || \
+    -z "${bdb_backend_host}" || \
+    -z "${bdb_backend_port}" ]] ; then
   echo "Invalid environment settings detected. Exiting!"
   exit 1
 fi
@@ -15,15 +23,15 @@ fi
 NGINX_CONF_FILE=/etc/nginx/nginx.conf
 
 # configure the nginx.conf file with env variables
-sed -i "s|MONGODB_FRONTEND_PORT|${MONGODB_FRONTEND_PORT}|g" $NGINX_CONF_FILE
-sed -i "s|MONGODB_BACKEND_HOST|${MONGODB_BACKEND_HOST}|g" $NGINX_CONF_FILE
-sed -i "s|MONGODB_BACKEND_PORT|${MONGODB_BACKEND_PORT}|g" $NGINX_CONF_FILE
-sed -i "s|BIGCHAINDB_FRONTEND_PORT|${BIGCHAINDB_FRONTEND_PORT}|g" $NGINX_CONF_FILE
-sed -i "s|BIGCHAINDB_BACKEND_HOST|${BIGCHAINDB_BACKEND_HOST}|g" $NGINX_CONF_FILE
-sed -i "s|BIGCHAINDB_BACKEND_PORT|${BIGCHAINDB_BACKEND_PORT}|g" $NGINX_CONF_FILE
+sed -i "s|MONGODB_FRONTEND_PORT|${mongo_frontend_port}|g" $NGINX_CONF_FILE
+sed -i "s|MONGODB_BACKEND_HOST|${mongo_backend_host}|g" $NGINX_CONF_FILE
+sed -i "s|MONGODB_BACKEND_PORT|${mongo_backend_port}|g" $NGINX_CONF_FILE
+sed -i "s|BIGCHAINDB_FRONTEND_PORT|${bdb_frontend_port}|g" $NGINX_CONF_FILE
+sed -i "s|BIGCHAINDB_BACKEND_HOST|${bdb_backend_host}|g" $NGINX_CONF_FILE
+sed -i "s|BIGCHAINDB_BACKEND_PORT|${bdb_backend_port}|g" $NGINX_CONF_FILE
 
 # populate the whitelist in the conf file as per MONGODB_WHITELIST env var
-hosts=$(echo ${MONGODB_WHITELIST} | tr ":" "\n")
+hosts=$(echo ${mongo_whitelist} | tr ":" "\n")
 for host in $hosts; do
   sed -i "s|MONGODB_WHITELIST|allow ${host};\n    MONGODB_WHITELIST|g" $NGINX_CONF_FILE
 done
