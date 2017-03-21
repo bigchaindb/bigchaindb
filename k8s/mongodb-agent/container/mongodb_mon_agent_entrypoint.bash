329013757044a560ca808b7cfccd9b2eacd8abd7
krish7919 (Krish) <krishnan.k.iyer@gmail.com>
2017-03-21 14:34:12 +0100

    Container for mongo monitoring agent - Removing REFRESHED_AT in Dockerfile as docker image tagging does the same thing - Updated the FILE_URL in Dockerfile with the correct URL - Installing more dependencies in Dockerfile for mongo monitoring agent

---
+++ k8s/mongodb-agent/container/mongodb_mon_agent_entrypoint.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+# -e Abort at the first failed line (i.e. if exit status is not 0)
+# -u Abort when undefined variable is used
+# -o pipefail (Bash-only) Piped commands return the status
+#    of the last failed command, rather than the status of the last command
+
+MONGODB_MON_CONF_FILE=/etc/mongodb-mms/monitoring-agent.config
+
+mms_api_key=`printenv MMS_API_KEY`
+mongo_host=`printenv MONGO_HOST`
+
+if [[ -z "${mms_api_key}" || -z "${mongo_host}" ]]; then
+  echo "Invalid environment settings detected. Exiting!"
+  exit 1
+fi
+
+# Delete all lines containing "mmsApiKey" in the MongoDB Monitoring Agent
+# config file /etc/mongodb-mms/monitoring-agent.config
+sed -i '/mmsApiKey/d'  $MONGODB_MON_CONF_FILE
+
+# Append a new line of the form
+# mmsApiKey=value_of_MMS_API_KEY
+echo "mmsApiKey="${mms_api_key} >> $MONGODB_MON_CONF_FILE
+
+# start mdb monitoring agent
+echo "INFO: starting mdb monitor..."
+exec mongodb-mms-monitoring-agent --conf /etc/mongodb-mms/monitoring-agent.config 
