0cbf144ddf364813d2651793494ad71ab0d58553
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-03-28 16:05:44 +0200

    Initial implementation of an event_handler

--- bigchaindb/processes.py
+++ bigchaindb/processes.py
@@ -3,6 +3,8 @@ import multiprocessing as mp
 
 import bigchaindb
 from bigchaindb.pipelines import vote, block, election, stale
+from bigchaindb.pipelines.events_consumer_example import events_consumer
+from bigchaindb.events import setup_events_queue
 from bigchaindb.web import server
 
 
@@ -25,6 +27,13 @@ BANNER = """
 def start():
     logger.info('Initializing BigchainDB...')
 
+    # Create the events queue
+    # The events queue needs to be initialized once and shared between
+    # processes. This seems the best way to do it
+    # At this point only the election processs and the event consumer require
+    # this queue.
+    events_queue = setup_events_queue()
+
     # start the processes
     logger.info('Starting block')
     block.start()
@@ -36,12 +45,17 @@ def start():
     stale.start()
 
     logger.info('Starting election')
-    election.start()
+    election.start(events_queue)
 
     # start the web api
     app_server = server.create_server(bigchaindb.config['server'])
     p_webapi = mp.Process(name='webapi', target=app_server.run)
     p_webapi.start()
+
+    # start the example events consumer
+    logger.info('Starting the events consumer example')
+    p_events_consumer = events_consumer(events_queue)
+    p_events_consumer.start()
 
     # start message
     logger.info(BANNER.format(bigchaindb.config['server']['bind']))
