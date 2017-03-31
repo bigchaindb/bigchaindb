75f5d03686858d901bcd0ec3e9987442f8a33461
vrde <agranzot@gmail.com>
2017-03-31 15:56:29 +0200

    Add more tests and utils

--- bigchaindb/web/websocket_server.py
+++ bigchaindb/web/websocket_server.py
@@ -1,29 +1,48 @@
 """WebSocket server for the BigchainDB Event Stream API."""
 
 import asyncio
+import logging
 from uuid import uuid4
 
+import aiohttp
 from aiohttp import web
 
 
-class PoisonPill:
-    pass
-
-
-POISON_PILL = PoisonPill()
+logger = logging.getLogger(__name__)
+POISON_PILL = 'POISON_PILL'
 
 
 class Dispatcher:
+    """Dispatch events to websockets.
+
+    This class implements a simple publish/subscribe pattern.
+    """
 
     def __init__(self, event_source):
+        """Create a new instance.
+
+        Args:
+            event_source: a source of events. Elements in the queue
+            should be strings.
+        """
+
         self.event_source = event_source
         self.subscribers = {}
 
     def subscribe(self, uuid, ws):
+        """Add a websocket to the list of subscribers.
+
+        Args:
+            uuid (str): a unique identifier for the websocket.
+            ws: the websocket to publish information.
+        """
+
         self.subscribers[uuid] = ws
 
     @asyncio.coroutine
     def publish(self):
+        """Publish new events to the subscribers."""
+
         while True:
             event = yield from self.event_source.get()
             if event == POISON_PILL:
@@ -34,17 +53,29 @@ class Dispatcher:
 
 @asyncio.coroutine
 def websocket_handler(request):
+    """Handle a new socket connection."""
+
+    logger.debug('New websocket connection.')
     ws = web.WebSocketResponse()
     yield from ws.prepare(request)
     uuid = uuid4()
     request.app['dispatcher'].subscribe(uuid, ws)
+
     while True:
         # Consume input buffer
-        yield from ws.receive()
-    return ws
+        msg = yield from ws.receive()
+        if msg.type == aiohttp.WSMsgType.ERROR:
+            logger.debug('Websocket exception: {}'.format(ws.exception()))
+            return
 
 
 def init_app(event_source, loop=None):
+    """Init the application server.
+
+    Return:
+        An aiohttp application.
+    """
+
     dispatcher = Dispatcher(event_source)
 
     # Schedule the dispatcher
@@ -54,3 +85,19 @@ def init_app(event_source, loop=None):
     app['dispatcher'] = dispatcher
     app.router.add_get('/', websocket_handler)
     return app
+
+
+@asyncio.coroutine
+def constant_event_source(event_source):
+    while True:
+        yield from asyncio.sleep(1)
+        yield from event_source.put('meow')
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    event_source = asyncio.Queue()
+
+    loop.create_task(constant_event_source(event_source))
+    app = init_app(event_source, loop=loop)
+    aiohttp.web.run_app(app, port=9985)
--- tests/web/test_websocket_server.py
+++ tests/web/test_websocket_server.py
@@ -1,4 +1,39 @@
+import pytest
 import asyncio
+
+
+class MockWebSocket:
+    def __init__(self):
+        self.received = []
+
+    def send_str(self, s):
+        self.received.append(s)
+
+
+@asyncio.coroutine
+@pytest.mark.skipif(reason='This test raises a RuntimeError, dunno how to solve it now.')
+def test_dispatcher(loop):
+    from bigchaindb.web.websocket_server import Dispatcher, POISON_PILL
+
+    ws0 = MockWebSocket()
+    ws1 = MockWebSocket()
+
+    event_source = asyncio.Queue(loop=loop)
+    dispatcher = Dispatcher(event_source)
+
+    dispatcher.subscribe(0, ws0)
+    dispatcher.subscribe(1, ws1)
+
+    yield from event_source.put('hack')
+    yield from event_source.put('the')
+
+    yield from event_source.put('planet!')
+    yield from event_source.put(POISON_PILL)
+
+    loop.run_until_complete(dispatcher.publish())
+
+    assert ws0.received == ['hack', 'the', 'planet!']
+    assert ws1.received == ['planet!']
 
 
 @asyncio.coroutine
@@ -9,7 +44,33 @@ def test_websocket(test_client, loop):
     app = init_app(event_source, loop=loop)
     client = yield from test_client(app)
     ws = yield from client.ws_connect('/')
-    yield from event_source.put('antani')
-    yield from event_source.put(POISON_PILL)
+
+    yield from event_source.put('hack')
+    yield from event_source.put('the')
+    yield from event_source.put('planet!')
+
     result = yield from ws.receive()
-    assert result.data == 'antani'
+    assert result.data == 'hack'
+
+    result = yield from ws.receive()
+    assert result.data == 'the'
+
+    result = yield from ws.receive()
+    assert result.data == 'planet!'
+
+    yield from event_source.put(POISON_PILL)
+
+
+@asyncio.coroutine
+@pytest.mark.skipif(reason="Still don't understand how to trigger custom errors.")
+def test_websocket_error(test_client, loop):
+    from bigchaindb.web.websocket_server import init_app, POISON_PILL
+
+    event_source = asyncio.Queue(loop=loop)
+    app = init_app(event_source, loop=loop)
+    client = yield from test_client(app)
+    ws = yield from client.ws_connect('/')
+
+    yield from ws.close()
+
+    yield from event_source.put(POISON_PILL)
