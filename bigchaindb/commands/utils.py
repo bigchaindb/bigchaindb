ca200b1da7ba77f9aa753fd20b8eb09ea4a95838
Jack Riches <jackriches@gmail.com>
2017-04-02 12:22:56 +0100

    Treat --log-level argument as case-insensitive

--- bigchaindb/commands/utils.py
+++ bigchaindb/commands/utils.py
@@ -198,6 +198,7 @@ base_parser.add_argument('-c', '--config',
                               '(use "-" for stdout)')
 
 base_parser.add_argument('-l', '--log-level',
+                         type=lambda l: l.upper(),  # case insensitive conversion
                          choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                          default='INFO',
                          help='Log level')
