d57ddee41174500e1765fc2e089e95b06d83d3c2
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-04-03 14:31:38 +0200

    cleanup code

--- bigchaindb/events.py
+++ bigchaindb/events.py
@@ -7,14 +7,14 @@ class EventTypes(Enum):
     BLOCK_INVALID = 2
 
 
-class Event(object):
+class Event:
 
     def __init__(self, event_type, event_data):
         self.type = event_type
         self.data = event_data
 
 
-class EventHandler(object):
+class EventHandler:
 
     def __init__(self, events_queue):
         self.events_queue = events_queue
