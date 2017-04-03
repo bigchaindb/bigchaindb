188b9da634208a0b78d1e57a626a1b7e1c96043d
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-04-03 14:29:31 +0200

    fix tests

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from bigchaindb.pipelines import vote, block, election, stale
 
 
@@ -9,14 +9,16 @@ from bigchaindb.pipelines import vote, block, election, stale
 @patch.object(block, 'start')
 @patch.object(vote, 'start')
 @patch.object(Process, 'start')
-def test_processes_start(mock_vote, mock_block, mock_election, mock_stale,
-                         mock_process):
+def test_processes_start(mock_process, mock_vote, mock_block, mock_election,
+                         mock_stale):
     from bigchaindb import processes
 
     processes.start()
 
     mock_vote.assert_called_with()
     mock_block.assert_called_with()
-    mock_election.assert_called_with()
     mock_stale.assert_called_with()
     mock_process.assert_called_with()
+    assert mock_election.call_count == 1
+    # the events queue is declared inside processes.start()
+    assert type(mock_election.call_args[1]['events_queue']) == type(Queue())
