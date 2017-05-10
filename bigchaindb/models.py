b41563af253aac899d45c337fee52f201a157e82
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-05-10 17:55:43 +0200

    Added asset decoupling support for rethinkdb
    Updated schema.
    Created queries for assets table.
    Fixed tests.

--- bigchaindb/models.py
+++ bigchaindb/models.py
@@ -90,7 +90,8 @@ class Transaction(Transaction):
     def from_db(cls, bigchain, tx_dict):
         # TODO: write docstring
         if tx_dict['operation'] in [Transaction.CREATE, Transaction.CREATE]:
-            asset = bigchain.get_assets([tx_dict['id']])[0]
+            # TODO: Maybe replace this call to a call to get_asset_by_id
+            asset = list(bigchain.get_assets([tx_dict['id']]))[0]
             asset.pop('id')
             tx_dict.update({'asset': asset})
 
