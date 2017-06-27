3f6bc43bc82720b2e40e8d54c09a19fa1c1d52b9
Sylvain Bellemare <sylvain@ascribe.io>
2017-06-27 18:53:00 +0200

    Refine the condition uri regex for validation
    closes #1516

--- tests/common/test_schema.py
+++ tests/common/test_schema.py
@@ -3,13 +3,20 @@ This module is tests related to schema checking, but _not_ of granular schematic
 properties related to validation.
 """
 
-from pytest import raises
 from unittest.mock import patch
+
+from hypothesis import given
+from hypothesis_regex import regex
+from pytest import raises
 
 from bigchaindb.common.exceptions import SchemaValidationError
 from bigchaindb.common.schema import (
     TX_SCHEMA_COMMON, VOTE_SCHEMA, drop_schema_descriptions,
     validate_transaction_schema, validate_vote_schema)
+
+SUPPORTED_CRYPTOCONDITION_TYPES = ('threshold-sha-256', 'ed25519-sha-256')
+UNSUPPORTED_CRYPTOCONDITION_TYPES = (
+    'preimage-sha-256', 'prefix-sha-256', 'rsa-sha-256')
 
 
 ################################################################################
@@ -107,6 +114,63 @@ def test_validate_failure_inconsistent():
     with patch('jsonschema.validate'):
         with raises(SchemaValidationError):
             validate_transaction_schema({})
+
+
+@given(condition_uri=regex(
+    r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{{0,86}})\?fpt=({})'
+    r'&cost=[0-9]+(?![\n])$'.format('|'.join(
+        t for t in SUPPORTED_CRYPTOCONDITION_TYPES))))
+def test_condition_uri_with_supported_fpt(dummy_transaction, condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    validate_transaction_schema(dummy_transaction)
+
+
+@given(condition_uri=regex(r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{{0,86}})\?fpt='
+                           r'({})&cost=[0-9]+(?![\n])$'.format(
+                               '|'.join(UNSUPPORTED_CRYPTOCONDITION_TYPES))))
+def test_condition_uri_with_unsupported_fpt(dummy_transaction, condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    with raises(SchemaValidationError):
+        validate_transaction_schema(dummy_transaction)
+
+
+@given(condition_uri=regex(
+    r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{{0,86}})\?fpt=(?!{})'
+    r'&cost=[0-9]+(?![\n])$'.format('$|'.join(
+        t for t in SUPPORTED_CRYPTOCONDITION_TYPES))))
+def test_condition_uri_with_unknown_fpt(dummy_transaction, condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    with raises(SchemaValidationError):
+        validate_transaction_schema(dummy_transaction)
+
+
+@given(condition_uri=regex(
+    r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{0,86})\?fpt=threshold-sha-256'
+    r'&cost=[0-9]+&subtypes=ed25519-sha-256(?![\n])$'))
+def test_condition_uri_with_supported_subtype(dummy_transaction,
+                                              condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    validate_transaction_schema(dummy_transaction)
+
+
+@given(condition_uri=regex(
+    r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{0,86})\?fpt=threshold-sha-256&cost='
+    r'[0-9]+&subtypes=(preimage-sha-256|prefix-sha-256|rsa-sha-256)(?![\n])$'))
+def test_condition_uri_with_unsupported_subtype(dummy_transaction,
+                                                condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    with raises(SchemaValidationError):
+        validate_transaction_schema(dummy_transaction)
+
+
+@given(condition_uri=regex(
+    r'^ni:\/\/\/sha-256;([a-zA-Z0-9_-]{{0,86}})\?fpt=threshold-sha-256'
+    r'&cost=[0-9]+&subtypes=(?!{})(?![\n])$'.format('$|'.join(
+        t for t in SUPPORTED_CRYPTOCONDITION_TYPES))))
+def test_condition_uri_with_unknown_subtype(dummy_transaction, condition_uri):
+    dummy_transaction['outputs'][0]['condition']['uri'] = condition_uri
+    with raises(SchemaValidationError):
+        validate_transaction_schema(dummy_transaction)
 
 
 ################################################################################
