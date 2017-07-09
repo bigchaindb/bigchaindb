684fffe917b18db7f607fe15662e3c35508b6f63
Ketan Bhatt <ketanbhatt1006@gmail.com>
2017-07-09 16:44:21 +0530

    HTTP API Logger logs request path and method too

--- bigchaindb/web/views/base.py
+++ bigchaindb/web/views/base.py
@@ -3,7 +3,7 @@ Common classes and methods for API handlers
 """
 import logging
 
-from flask import jsonify
+from flask import jsonify, request
 
 from bigchaindb import config
 
@@ -14,8 +14,13 @@ logger = logging.getLogger(__name__)
 def make_error(status_code, message=None):
     if status_code == 404 and message is None:
         message = 'Not found'
+
     response_content = {'status': status_code, 'message': message}
-    logger.error('HTTP API error: %(status)s - %(message)s', response_content)
+    request_info = {'method': request.method, 'path': request.path}
+    request_info.update(response_content)
+
+    logger.error('HTTP API error: %(status)s - %(method)s:%(path)s - %(message)s', request_info)
+
     response = jsonify(response_content)
     response.status_code = status_code
     return response
