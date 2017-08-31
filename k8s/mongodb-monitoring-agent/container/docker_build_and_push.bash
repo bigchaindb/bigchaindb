d183d7133a52bd38b304804845a561d5a812501b
krish7919 (Krish) <krishnan.k.iyer@gmail.com>
2017-08-31 15:52:28 +0200

    Update mongodb monitoring agent to latest version.

--- k8s/mongodb-monitoring-agent/container/docker_build_and_push.bash
+++ k8s/mongodb-monitoring-agent/container/docker_build_and_push.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t bigchaindb/mongodb-monitoring-agent:3.0 .
+docker build -t bigchaindb/mongodb-monitoring-agent:3.1 .
 
-docker push bigchaindb/mongodb-monitoring-agent:3.0
+docker push bigchaindb/mongodb-monitoring-agent:3.1
