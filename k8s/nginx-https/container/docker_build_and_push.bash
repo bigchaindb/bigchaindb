4371a2ce4b6e76ed322520ef3070074e10dc8591
muawiakh <amuawiakhan@gmail.com>
2017-09-26 14:19:55 +0200

    Update nginx config for well being
    - turned off server tokens so the server does not leak nginx information
      on errors and header
    - Added header to turn off cross site scripting
    - use stable release of nginx instead of mainline
    - limit available methods
    - update response code

--- k8s/nginx-https/container/docker_build_and_push.bash
+++ k8s/nginx-https/container/docker_build_and_push.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t bigchaindb/nginx_https:1.0 .
+docker build -t bigchaindb/nginx_https:1.1 .
 
-docker push bigchaindb/nginx_https:1.0
+docker push bigchaindb/nginx_https:1.1
