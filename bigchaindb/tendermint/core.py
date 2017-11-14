7f2a0595fefdd9a5cd96ac14f6114ca28b1be72e
kansi <kansi13@gmail.com>
2017-11-14 17:05:55 +0530

    Aggregate transaction ids for commit hash

--- bigchaindb/tendermint/core.py
+++ bigchaindb/tendermint/core.py
@@ -5,7 +5,7 @@ with Tendermint."""
 from abci import BaseApplication, Result
 
 from bigchaindb.tendermint import BigchainDB
-from bigchaindb.tendermint.utils import decode_transaction
+from bigchaindb.tendermint.utils import decode_transaction, calculate_hash
 
 
 class App(BaseApplication):
@@ -19,6 +19,7 @@ class App(BaseApplication):
         if not bigchaindb:
             bigchaindb = BigchainDB()
         self.bigchaindb = bigchaindb
+        self.block_txn_ids = []
 
     def check_tx(self, raw_transaction):
         """Validate the transaction before entry into
@@ -33,6 +34,9 @@ class App(BaseApplication):
         else:
             return Result.error()
 
+    def begin_block(self, block_hash, header):
+        self.block_txn_ids = []
+
     def deliver_tx(self, raw_transaction):
         """Validate the transaction before mutating the state.
 
@@ -46,4 +50,11 @@ class App(BaseApplication):
             return Result.error(log='Invalid transaction')
         else:
             self.bigchaindb.store_transaction(transaction)
+            self.block_txn_ids.append(transaction.to_dict()['id'])
             return Result.ok()
+
+    def commit(self):
+        """ Return merkle root of the transactions included in the
+        block"""
+        merkle_root = calculate_hash(self.block_txn_ids)
+        return Result.ok(data=merkle_root.encode('utf-8'))
--- bigchaindb/tendermint/utils.py
+++ bigchaindb/tendermint/utils.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import hashlib
 
 
 def encode_transaction(value):
@@ -12,3 +13,11 @@ def decode_transaction(raw):
     """Decode a transaction from Base64 to a dict."""
 
     return json.loads(raw.decode('utf8'))
+
+
+def calculate_hash(key_list):
+    full_hash = hashlib.sha256()
+    for key in key_list:
+        full_hash.update(key)
+
+    return full_hash.hexdigest()
