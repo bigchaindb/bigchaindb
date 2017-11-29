21c67da6e083c0ceee46fb379403dcb328e5433a
codegeschrei <katharina.griess@gmail.com>
2017-11-29 15:52:25 +0100

    add tests to improve codecov

--- tests/tendermint/test_lib.py
+++ tests/tendermint/test_lib.py
@@ -42,10 +42,9 @@ def test_validation_error(b):
     from bigchaindb.common.crypto import generate_key_pair
 
     alice = generate_key_pair()
-    asset = {'': ''}
     tx = Transaction.create([alice.public_key],
                             [([alice.public_key], 1)],
-                            asset=asset)\
+                            asset=None)\
                     .sign([alice.private_key]).to_dict()
 
     tx['metadata'] = ''
@@ -69,6 +68,9 @@ def test_write_and_post_transaction(mock_post, b):
     b.write_transaction(tx)
 
     assert mock_post.called
-    assert 'broadcast_tx_async' in str(mock_post.call_args)
+    args, kwargs = mock_post.call_args
+    assert 'broadcast_tx_async' in kwargs['json']['method']
     encoded_tx = encode_transaction(tx.to_dict())
-    assert encoded_tx in str(mock_post.call_args)
+    assert encoded_tx in kwargs['json']['params']
+    import pdb; pdb.set_trace()
+    assert kwargs['json']['id']
