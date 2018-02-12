adc7c01b75fdb167c751742c435402152dab91db
Sylvain Bellemare <sbellem@gmail.com>
2018-02-12 18:03:40 +0100

    Problem: Tendermint is not part of stack for CI
    Solution: Add Tendermint to stack for CI. For simplicity's sake
    docker-compose is being used.

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -2,27 +2,6 @@
 
 set -e -x
 
-if [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
-        -z "${BIGCHAINDB_DATABASE_SSL}" ]]; then
-    # Connect to MongoDB on port 27017 via a normal, unsecure connection if
-    # BIGCHAINDB_DATABASE_SSL is unset.
-    # It is unset in this case in .travis.yml.
-    docker pull mongo:3.4
-    docker run -d --publish=27017:27017 --name mdb-without-ssl mongo:3.4 # --replSet=bigchain-rs
-elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == localmongodb && \
-        "${BIGCHAINDB_DATABASE_SSL}" == true ]]; then
-    # Connect to MongoDB on port 27017 via TLS/SSL connection if
-    # BIGCHAINDB_DATABASE_SSL is set.
-    # It is set to 'true' here in .travis.yml. Dummy certificates for testing
-    # are stored under bigchaindb/tests/backend/mongodb-ssl/certs/ directory.
-    docker pull mongo:3.4
-    docker run -d \
-        --name mdb-with-ssl \
-        --publish=27017:27017 \
-        --volume=${TRAVIS_BUILD_DIR}/tests/backend/mongodb-ssl/certs:/certs \
-        mongo:3.4 \
-        --sslMode=requireSSL \
-        --sslCAFile=/certs/ca-chain.cert.pem \
-        --sslCRLFile=/certs/crl.pem \
-        --sslPEMKeyFile=/certs/local-mongo.pem
+if [[ -z ${TOXENV} ]]; then
+    docker-compose -f docker-compose.travis.yml up -d bdb
 fi
