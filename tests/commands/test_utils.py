5cea9c28bf5833c3aa23e9001cc0ee062c0cdfb5
Ahmed Muawia Khan <amuawiakhan@gmail.com>
2018-02-22 21:34:31 +0100

    Migrate CLI for tendermint integration
    - Remove commands that were not required.
      - export-my-pubkey
      - set-shards
      - set-replicas
      - add-replicas
      - remove-replicas
    - Update bigchaindb --help, usage description
    - Re-enable tests
    - Update docker-compose.travis.yml and Dockerfile for travis
      because some env variables are not needed

--- tests/commands/test_utils.py
+++ tests/commands/test_utils.py
@@ -119,20 +119,5 @@ def test_start_sets_multiprocess_var_based_on_cli_args(mock_cpu_count):
 
     scope = {'run_mp_arg_test': run_mp_arg_test}
     assert utils.start(parser, ['mp_arg_test'], scope).multiprocess == 1
-    assert utils.start(parser, ['mp_arg_test', '--multiprocess'], scope).multiprocess == 42
-
-
-def test_mongodb_host_type():
-    from bigchaindb.commands.utils import mongodb_host
-
-    # bad port provided
-    with pytest.raises(ArgumentTypeError):
-        mongodb_host('localhost:11111111111')
-
-    # no port information provided
-    with pytest.raises(ArgumentTypeError):
-        mongodb_host('localhost')
-
-    # bad host provided
-    with pytest.raises(ArgumentTypeError):
-        mongodb_host(':27017')
+    assert utils.start(
+        parser, ['mp_arg_test', '--multiprocess'], scope).multiprocess == 42
