d9413acb9b54bee9d56ed72a7b1396f955778fd0
Ahmed Muawia Khan <amuawiakhan@gmail.com>
2018-03-05 21:17:25 +0100

    Typo tendermint*

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -7,6 +7,6 @@ pip install --upgrade pip
 if [[ -n ${TOXENV} ]]; then
     pip install --upgrade tox
 else
-    docker-compose -f docker-compose.tendemrint.yml build --no-cache
+    docker-compose -f docker-compose.tendermint.yml build --no-cache
     pip install --upgrade codecov
 fi
