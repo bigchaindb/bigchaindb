6402267d82785b603f04028d52e931d1df647058
kansi <kansi13@gmail.com>
2018-03-18 07:35:39 +0530

    Problem: Tendermint cannot connect to BDB
    Solution: Fix docker-compose.travis.yml

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -5,7 +5,7 @@ set -e -x
 if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ ${BIGCHAINDB_CI_ABCI} == 'enable' ]]; then
-  docker-compose -f docker-compose.travis.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb -m abci
+  docker-compose -f docker-compose.travis.yml exec bdb pytest -v -m abci
 else
   docker-compose -f docker-compose.travis.yml run --rm --no-deps bdb pytest -v --cov=bigchaindb
 fi
