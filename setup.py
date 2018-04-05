d28b9019c08495789f86b81ef8283865fb031184
Vanshdeep Singh <kansi13@gmail.com>
2018-04-05 15:29:32 +0200

    Problem: Python version not check before running (#2179)
    Solution: Check python version

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ For full docs visit https://docs.bigchaindb.com
 
 """
 from setuptools import setup, find_packages
+import sys
 
+
+if sys.version_info < (3, 6):
+    sys.exit('Please use python version 3.6 or higher')
 
 # get the version
 version = {}
@@ -107,7 +111,7 @@ setup(
     author_email='dev@bigchaindb.com',
     license='Apache Software License 2.0',
     zip_safe=False,
-
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
