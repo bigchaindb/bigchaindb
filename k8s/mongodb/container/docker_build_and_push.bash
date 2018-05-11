e1e06ac5b0f14620b3ebdb3a281ec69c39839227
Ahmed Muawia Khan <amuawiakhan@gmail.com>
2018-05-11 15:04:49 +0200

    Problem: Missing alpha5 image update for test network containers

--- k8s/mongodb/container/docker_build_and_push.bash
+++ k8s/mongodb/container/docker_build_and_push.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t bigchaindb/localmongodb:2.0.0-alpha3 .
-docker push bigchaindb/localmongodb:2.0.0-alpha3
+docker build -t bigchaindb/localmongodb:2.0.0-alpha5 .
+docker push bigchaindb/localmongodb:2.0.0-alpha5
