79052cbc39a87584f771bf9686a4dc036071e083
vrde <agranzot@gmail.com>
2018-05-17 12:27:22 +0200

    Problem: BigchainDB does not support newer MongoDB
    Solution: Update driver to the last version, that is compatible with all
    versions of MongoDB

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ benchmarks_require = [
 install_requires = [
     # TODO Consider not installing the db drivers, or putting them in extras.
     'rethinkdb~=2.3',  # i.e. a version between 2.3 and 3.0
-    'pymongo~=3.4',
+    'pymongo~=3.6',
     'pysha3~=1.0.2',
     'cryptoconditions~=0.6.0.dev',
     'python-rapidjson==0.0.11',
