f6815d35e427f9d3f04b91b113b52cd8eedca19a
Ahmed Muawia Khan <amuawiakhan@gmail.com>
2018-06-04 15:59:03 +0200

    Problem: Docker images of tendermint/tendermint:0.19.7 tagged but not updated in bdb

--- pkg/scripts/unstack.sh
+++ pkg/scripts/unstack.sh
@@ -11,7 +11,7 @@ stack_repo=${STACK_REPO:="bigchaindb/bigchaindb"}
 stack_size=${STACK_SIZE:=4}
 stack_type=${STACK_TYPE:="docker"}
 stack_type_provider=${STACK_TYPE_PROVIDER:=""}
-tm_version=${TM_VERSION:="0.19.2"}
+tm_version=${TM_VERSION:="0.19.7"}
 mongo_version=${MONGO_VERSION:="3.6"}
 stack_vm_memory=${STACK_VM_MEMORY:=2048}
 stack_vm_cpus=${STACK_VM_CPUS:=2}
