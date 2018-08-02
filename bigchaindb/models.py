1a74afa9cdc213bb6a62bcf15e6c6b8b1733afd7
Vanshdeep Singh <kansi13@gmail.com>
2018-08-02 11:49:59 +0200

    Problem: Node operator cannot vote on a ValidatorElection (#2428)
    * Problem: Node operator cannot vote on a ValidatorElection
    
    Solution: Implement validator election voting spec
    
    * Problem: Incorrent code comments
    
    Solution: Update comments with correct context
    
    * Problem: Delegated vote not casted back to election
    
    Solution: Update test to cast votes back to election id and assert their validity

--- bigchaindb/models.py
+++ bigchaindb/models.py
@@ -1,7 +1,4 @@
-from bigchaindb.common.exceptions import (InvalidSignature, DoubleSpend,
-                                          InputDoesNotExist,
-                                          TransactionNotInValidBlock,
-                                          AssetIdMismatch, AmountError,
+from bigchaindb.common.exceptions import (InvalidSignature,
                                           DuplicateTransaction)
 from bigchaindb.common.transaction import Transaction
 from bigchaindb.common.utils import (validate_txn_obj, validate_key)
@@ -32,64 +29,12 @@ class Transaction(Transaction):
             if bigchain.get_transaction(self.to_dict()['id']) or duplicates:
                 raise DuplicateTransaction('transaction `{}` already exists'
                                            .format(self.id))
+
+            if not self.inputs_valid(input_conditions):
+                raise InvalidSignature('Transaction signature is invalid.')
+
         elif self.operation == Transaction.TRANSFER:
-            # store the inputs so that we can check if the asset ids match
-            input_txs = []
-            for input_ in self.inputs:
-                input_txid = input_.fulfills.txid
-                input_tx, status = bigchain.\
-                    get_transaction(input_txid, include_status=True)
-
-                if input_tx is None:
-                    for ctxn in current_transactions:
-                        # assume that the status as valid for previously validated
-                        # transactions in current round
-                        if ctxn.id == input_txid:
-                            input_tx = ctxn
-                            status = bigchain.TX_VALID
-
-                if input_tx is None:
-                    raise InputDoesNotExist("input `{}` doesn't exist"
-                                            .format(input_txid))
-
-                if status != bigchain.TX_VALID:
-                    raise TransactionNotInValidBlock(
-                        'input `{}` does not exist in a valid block'.format(
-                            input_txid))
-
-                spent = bigchain.get_spent(input_txid, input_.fulfills.output,
-                                           current_transactions)
-                if spent:
-                    raise DoubleSpend('input `{}` was already spent'
-                                      .format(input_txid))
-
-                output = input_tx.outputs[input_.fulfills.output]
-                input_conditions.append(output)
-                input_txs.append(input_tx)
-
-            # Validate that all inputs are distinct
-            links = [i.fulfills.to_uri() for i in self.inputs]
-            if len(links) != len(set(links)):
-                raise DoubleSpend('tx "{}" spends the same output more than once'.format(self.id))
-
-            # validate asset id
-            asset_id = Transaction.get_asset_id(input_txs)
-            if asset_id != self.asset['id']:
-                raise AssetIdMismatch(('The asset id of the input does not'
-                                       ' match the asset id of the'
-                                       ' transaction'))
-
-            input_amount = sum([input_condition.amount for input_condition in input_conditions])
-            output_amount = sum([output_condition.amount for output_condition in self.outputs])
-
-            if output_amount != input_amount:
-                raise AmountError(('The amount used in the inputs `{}`'
-                                   ' needs to be same as the amount used'
-                                   ' in the outputs `{}`')
-                                  .format(input_amount, output_amount))
-
-        if not self.inputs_valid(input_conditions):
-            raise InvalidSignature('Transaction signature is invalid.')
+            self.validate_transfer_inputs(bigchain, current_transactions)
 
         return self
 
---
+++ tests/upsert_validator/test_validator_election_vote.py
@@ -0,0 +1,80 @@
+import pytest
+
+from bigchaindb.upsert_validator import ValidatorElectionVote
+from bigchaindb.common.exceptions import AmountError
+
+
+pytestmark = [pytest.mark.tendermint, pytest.mark.bdb]
+
+
+def test_upsert_validator_valid_election_vote(b_mock, valid_election, ed25519_node_keys):
+    b_mock.store_bulk_transactions([valid_election])
+
+    input0 = valid_election.to_inputs()[0]
+    votes = valid_election.outputs[0].amount
+    public_key0 = input0.owners_before[0]
+    key0 = ed25519_node_keys[public_key0]
+
+    election_pub_key = ValidatorElectionVote.to_public_key(valid_election.id)
+
+    vote = ValidatorElectionVote.generate([input0],
+                                          [([election_pub_key], votes)],
+                                          election_id=valid_election.id)\
+                                .sign([key0.private_key])
+    assert vote.validate(b_mock)
+
+
+def test_upsert_validator_delegate_election_vote(b_mock, valid_election, ed25519_node_keys):
+    from bigchaindb.common.crypto import generate_key_pair
+
+    alice = generate_key_pair()
+
+    b_mock.store_bulk_transactions([valid_election])
+
+    input0 = valid_election.to_inputs()[0]
+    votes = valid_election.outputs[0].amount
+    public_key0 = input0.owners_before[0]
+    key0 = ed25519_node_keys[public_key0]
+
+    delegate_vote = ValidatorElectionVote.generate([input0],
+                                                   [([alice.public_key], 3), ([key0.public_key], votes-3)],
+                                                   election_id=valid_election.id)\
+                                         .sign([key0.private_key])
+
+    assert delegate_vote.validate(b_mock)
+
+    b_mock.store_bulk_transactions([delegate_vote])
+    election_pub_key = ValidatorElectionVote.to_public_key(valid_election.id)
+
+    alice_votes = delegate_vote.to_inputs()[0]
+    alice_casted_vote = ValidatorElectionVote.generate([alice_votes],
+                                                       [([election_pub_key], 3)],
+                                                       election_id=valid_election.id)\
+                                             .sign([alice.private_key])
+    assert alice_casted_vote.validate(b_mock)
+
+    key0_votes = delegate_vote.to_inputs()[1]
+    key0_casted_vote = ValidatorElectionVote.generate([key0_votes],
+                                                      [([election_pub_key], votes-3)],
+                                                      election_id=valid_election.id)\
+                                            .sign([key0.private_key])
+    assert key0_casted_vote.validate(b_mock)
+
+
+def test_upsert_validator_invalid_election_vote(b_mock, valid_election, ed25519_node_keys):
+    b_mock.store_bulk_transactions([valid_election])
+
+    input0 = valid_election.to_inputs()[0]
+    votes = valid_election.outputs[0].amount
+    public_key0 = input0.owners_before[0]
+    key0 = ed25519_node_keys[public_key0]
+
+    election_pub_key = ValidatorElectionVote.to_public_key(valid_election.id)
+
+    vote = ValidatorElectionVote.generate([input0],
+                                          [([election_pub_key], votes+1)],
+                                          election_id=valid_election.id)\
+                                .sign([key0.private_key])
+
+    with pytest.raises(AmountError):
+        assert vote.validate(b_mock)
