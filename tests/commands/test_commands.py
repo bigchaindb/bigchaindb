cb0dd5ecbe2b9c19b85c3bdd2a49aa47d3cca3ff
z-bowen <zach.bowen@gmail.com>
2018-08-08 16:10:23 +0200

    Problem: Removed argparse for `upsert-validator-show` command, but forgot to remove the test for these args
    Solution: Removed the test

--- tests/commands/test_commands.py
+++ tests/commands/test_commands.py
@@ -20,7 +20,6 @@ def test_make_sure_we_dont_remove_any_command():
     assert parser.parse_args(['start']).command
     assert parser.parse_args(['upsert-validator', 'new', 'TEMP_PUB_KEYPAIR', '10', 'TEMP_NODE_ID',
                               '--private-key', 'TEMP_PATH_TO_PRIVATE_KEY']).command
-    assert parser.parse_args(['upsert-validator', 'show', 'TEMP_ELECTION_ID']).command
 
 
 @pytest.mark.tendermint
