bdfa059046c8e6f4547eb3fd460a6bcc8ccfbced
codegeschrei <codegeschrei@gmail.com>
2018-08-08 12:17:44 +0200

    Problem: some tests are not activated (#2390)
    * Problem: core.py contains an unused class, `Bigchain`
    
    Solution: Remove core.py. Refactor BigchainDB Class to remove inheritance from Bigchain.
    
    * Problem: core.py contains an unused class, `Bigchain`
    
    Solution: Remove core.py. Refactor BigchainDB Class to remove inheritance from Bigchain.
    
    * Fixed flake8 complaint about too many blank lines
    
    * Attempting to fix Sphinx docs. This may result in some redundant commits, as I don't know what I'm doing, and I can't experiment without running the CI...
    
    Sorry in advance!
    
    * Attempting to fix Sphinx docs. This may result in some redundant commits, as I don't know what I'm doing, and I can't experiment without running the CI...
    
    Sorry in advance!
    
    * Updating from master changed BigchainDB.process_post_response to a private method, so I had to align with that.
    
    * Fixed a couple stale references to bigchaindb.Bigchain in docstrings
    
    * Missed a reference to `Bigchain` in a patch call...
    
    * Problem: some tests are not activated
    Solution: activate and remove or fix tests
    
    * Problem: accidentally un-skipped test_get_blocks_status_containing_tx during merge
    
    Solution: Replaced the skip

--- tests/test_txlist.py
+++ tests/test_txlist.py
@@ -4,46 +4,27 @@ This test module defines it's own fixture which is used by all the tests.
 """
 import pytest
 
+pytestmark = pytest.mark.tendermint
+
 
 @pytest.fixture
-def txlist(b, user_pk, user2_pk, user_sk, user2_sk, genesis_block):
+def txlist(b, user_pk, user2_pk, user_sk, user2_sk):
     from bigchaindb.models import Transaction
-    prev_block_id = genesis_block.id
 
-    # Create first block with CREATE transactions
+    # Create two CREATE transactions
     create1 = Transaction.create([user_pk], [([user2_pk], 6)]) \
         .sign([user_sk])
+
     create2 = Transaction.create([user2_pk],
                                  [([user2_pk], 5), ([user_pk], 5)]) \
-                         .sign([user2_sk])
-    block1 = b.create_block([create1, create2])
-    b.write_block(block1)
+        .sign([user2_sk])
 
-    # Create second block with TRANSFER transactions
+    # Create a TRANSFER transactions
     transfer1 = Transaction.transfer(create1.to_inputs(),
                                      [([user_pk], 8)],
                                      create1.id).sign([user2_sk])
-    block2 = b.create_block([transfer1])
-    b.write_block(block2)
 
-    # Create block with double spend
-    tx_doublespend = Transaction.transfer(create1.to_inputs(), [([user_pk], 9)],
-                                          create1.id).sign([user2_sk])
-    block_doublespend = b.create_block([tx_doublespend])
-    b.write_block(block_doublespend)
-
-    # Vote on all the blocks
-    prev_block_id = genesis_block.id
-    for bid in [block1.id, block2.id]:
-        vote = b.vote(bid, prev_block_id, True)
-        prev_block_id = bid
-        b.write_vote(vote)
-
-    # Create undecided block
-    untx = Transaction.create([user_pk], [([user2_pk], 7)]) \
-        .sign([user_sk])
-    block_undecided = b.create_block([untx])
-    b.write_block(block_undecided)
+    b.store_bulk_transactions([create1, create2, transfer1])
 
     return type('', (), {
         'create1': create1,
@@ -54,8 +35,8 @@ def txlist(b, user_pk, user2_pk, user_sk, user2_sk, genesis_block):
 @pytest.mark.bdb
 def test_get_txlist_by_asset(b, txlist):
     res = b.get_transactions_filtered(txlist.create1.id)
-    assert set(tx.id for tx in res) == set([txlist.transfer1.id,
-                                            txlist.create1.id])
+    assert sorted(set(tx.id for tx in res)) == sorted(
+        set([txlist.transfer1.id, txlist.create1.id]))
 
 
 @pytest.mark.bdb
