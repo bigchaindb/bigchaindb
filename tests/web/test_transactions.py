55a9151c1479f6ba907921ca879dd233a26f3c6c
codegeschrei <codegeschrei@gmail.com>
2018-08-22 09:56:44 +0200

    Problem: tests in test_bigchain_api are not activated (#2456)
    * Problem: tests in test_bigchain_api are not activated
    Solution: remove or active tests
    
    * Problem: TransactionNotInValidBlock was removed
    Solution: remove TransactionNotInValidBlock from tests

--- tests/web/test_transactions.py
+++ tests/web/test_transactions.py
@@ -265,7 +265,6 @@ def test_post_create_transaction_with_invalid_schema(mock_logger, client):
     ('ValidationError', 'Create and transfer!'),
     ('InputDoesNotExist', 'Hallucinations?'),
     ('TransactionOwnerError', 'Not yours!'),
-    ('TransactionNotInValidBlock', 'Wait, maybe?'),
     ('ValidationError', '?'),
 ))
 @patch('bigchaindb.web.views.base.logger')
