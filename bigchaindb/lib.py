b468504bfc6686d9211f642267076d03ba981419
z-bowen <zach.bowen@gmail.com>
2018-09-04 11:12:51 +0200

    Problem: Missed a method in `Bigchain` class when updating the naming for an election method
    Solution: Finished the refactoring

--- bigchaindb/lib.py
+++ bigchaindb/lib.py
@@ -429,7 +429,7 @@ class BigchainDB(object):
         validators = result['validators']
         return validators
 
-    def get_result_by_election_id(self, election_id, table):
+    def get_election_result_by_id(self, election_id, table):
         result = backend.query.get_election_result_by_id(self.connection, election_id, table)
         return result
 
