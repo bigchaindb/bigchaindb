c6d07f1c8f39bd6882b65fafe99ee90d81884832
Juergen Eckel <juergen@riddleandcode.com>
2019-10-08 00:21:44 +0200

    increased version number to 2.1.0
    Signed-off-by: Juergen Eckel <juergen@riddleandcode.com>

--- bigchaindb/version.py
+++ bigchaindb/version.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-__version__ = '2.0.0'
-__short_version__ = '2.0'
+__version__ = '2.1.0'
+__short_version__ = '2.1'
 
 # Supported Tendermint versions
 __tm_supported_versions__ = ["0.31.5"]
