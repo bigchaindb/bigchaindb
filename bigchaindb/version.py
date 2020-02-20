09391351a47c710b2305a8e6c645c6397daf9f98
David Dashyan <mail@davie.li>
2020-02-20 15:30:37 +0000

    Version bump to 2.2.0 (#2682)
    Signed-off-by: David Dashyan <mail@davie.li>

--- bigchaindb/version.py
+++ bigchaindb/version.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-__version__ = '2.1.0'
-__short_version__ = '2.1'
+__version__ = '2.2.0'
+__short_version__ = '2.2'
 
 # Supported Tendermint versions
 __tm_supported_versions__ = ["0.31.5", "0.22.8"]
